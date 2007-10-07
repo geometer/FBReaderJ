68061de6dab6ddbe2aa36e5ae1a6161e5b041c06
Nikolay Pultsin <geometer@fbreader.org>
2007-10-07 16:31:23 +0000

    Test commit from Eclipse.
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@3 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/zlibrary/model/ZLTextModel.java
+++ src/org/zlibrary/model/ZLTextModel.java
@@ -7,6 +7,8 @@ public interface ZLTextModel {
         PLAIN_TEXT_MODEL,
     };
     
+    // to be removed: in Java we can use 'instanceof' operator instead of this method
+    // -- NP
     Kind getKind();
     int getParagraphsNumber();
     ZLTextParagraph getParagraph(int index);
