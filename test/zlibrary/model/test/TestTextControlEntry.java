bd5d796e8c69d7d55d7b36068936fb6b78c0c4db
MarinaSokol <fbreader@fbreader.org>
2007-10-14 15:15:53 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@32 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ test/zlibrary/model/test/TestTextControlEntry.java
@@ -0,0 +1,17 @@
+package org.test.zlibrary.model;
+
+import org.zlibrary.model.entry.ZLTextControlEntry;
+import org.zlibrary.model.impl.ZLModelFactory;
+import junit.framework.TestCase;
+
+public class TestTextControlEntry extends TestCase {
+    private ZLModelFactory factory = new ZLModelFactory(); 
+	
+	public void test() {
+        boolean start = true;
+        byte kind = (byte)0;
+        ZLTextControlEntry entry = factory.createControlEntry(kind, start);
+        assertEquals(entry.getKind(), kind);
+        assertEquals(entry.isStart(), start);
+    }
+}
