0053227f4365751a25cba738dacfc1bebb25ba7f
MarinaSokol <fbreader@fbreader.org>
2007-10-14 15:15:27 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@30 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ test/zlibrary/model/test/TestTextEntry.java
@@ -0,0 +1,16 @@
+package org.test.zlibrary.model;
+
+import junit.framework.TestCase;
+import org.zlibrary.model.entry.ZLTextEntry;
+import org.zlibrary.model.impl.ZLModelFactory;
+
+public class TestTextEntry extends TestCase {
+    private ZLModelFactory factory = new ZLModelFactory(); 
+
+    public void test() {
+        String str = "marina";
+        ZLTextEntry entry = factory.createTextEntry(str);
+        assertEquals(entry.getData(), str);
+        assertEquals(entry.getDataLength(), str.length());
+    }
+}
