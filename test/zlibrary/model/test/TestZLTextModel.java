ba701fee42dd716cf19a9fd284d01f4b18e804a0
MarinaSokol <fbreader@fbreader.org>
2007-10-14 15:15:02 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@28 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ test/zlibrary/model/test/TestZLTextModel.java
@@ -0,0 +1,56 @@
+package org.test.zlibrary.model;
+
+import junit.framework.TestCase;
+import org.zlibrary.model.ZLTextModel;
+import org.zlibrary.model.ZLTextParagraph;
+import org.zlibrary.model.entry.ZLTextEntry;
+import org.zlibrary.model.impl.ZLModelFactory;
+
+public class TestZLTextModel extends TestCase {
+    private ZLModelFactory factory = new ZLModelFactory(); 
+    
+    public void testAddParagraph() {
+        ZLTextModel model = factory.createModel();
+        ZLTextParagraph paragraph = factory.createParagraph();
+        paragraph.addEntry(factory.createTextEntry("marina"));
+        model.addParagraphInternal(paragraph);
+        model.addParagraphInternal(factory.createParagraph());
+        assertEquals(model.getParagraphsNumber(), 2);
+        assertEquals(model.getParagraph(0), paragraph);
+    }
+
+    public void testAddEntry() {
+        ZLTextModel model = factory.createModel();
+        ZLTextParagraph paragraph = factory.createParagraph();
+        paragraph.addEntry(factory.createTextEntry("marina"));
+        model.addParagraphInternal(paragraph);
+        model.addParagraphInternal(factory.createParagraph());
+        assertEquals(model.getParagraphsNumber(), 2);
+        assertEquals(model.getParagraph(0), paragraph);
+        model.addText("addText");
+        assertEquals(((ZLTextEntry)model.getParagraph(1).getEntries().get(0)).getData(), "addText");
+        StringBuffer sb = new StringBuffer();
+        sb.append("1");
+        sb.append("2");
+        model.addText(sb);
+        assertEquals(((ZLTextEntry)model.getParagraph(1).getEntries().get(1)).getData(), "12");
+    }
+
+    public void testAddControl() {
+        ZLTextModel model = factory.createModel();
+        ZLTextParagraph paragraph = factory.createParagraph();
+        paragraph.addEntry(factory.createTextEntry("marina"));
+        model.addParagraphInternal(paragraph);
+        model.addParagraphInternal(factory.createParagraph());
+        assertEquals(model.getParagraphsNumber(), 2);
+        assertEquals(model.getParagraph(0), paragraph);
+        model.addControl((byte)0, true);
+        model.addControl((byte)0, false);
+        model.addControl((byte)1, true);
+        model.addControl((byte)1, false);
+        model.addControl((byte)0, true);
+        model.addControl((byte)0, false);
+        
+    }
+
+}
