a44c7e40ddf86b4c8556b467b83bf9d61aa1a906
tushkanchik <fbreader@fbreader.org>
2007-10-26 22:02:24 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@53 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- test/org/test/fbreader/formats/fb2/TestFB2Reader.java
+++ test/org/test/fbreader/formats/fb2/TestFB2Reader.java
@@ -1,6 +1,7 @@
 package org.test.fbreader.formats.fb2;
 
 import org.fbreader.formats.fb2.FB2Reader;
+import org.fbreader.formats.fb2.FB2Tag;
 import org.zlibrary.model.ZLTextModel;
 
 import junit.framework.TestCase;
@@ -18,5 +19,52 @@ public class TestFB2Reader extends TestCase {
 		assertEquals(model.dump(),
 				"[PARAGRAPH]\n[TEXT]Тест 2[/TEXT][/PARAGRAPH]\n[PARAGRAPH]\n[TEXT]Два абзаца[/TEXT][/PARAGRAPH]\n");
 	}
+	
+	public void testControlSup() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test3.fb2");
+		ZLTextModel model = reader.read();
+		int sup = FB2Tag.SUP.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + sup +
+				"][TEXT]3[/TEXT][/CONTROL " + sup + "][/PARAGRAPH]\n");
+	}
+	
+	public void testControlSub() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test4.fb2");
+		ZLTextModel model = reader.read();
+		int sub = FB2Tag.SUB.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + sub +
+				"][TEXT]4[/TEXT][/CONTROL " + sub + "][/PARAGRAPH]\n");
+	}
+	
+	public void testControlEmphasis() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test5.fb2");
+		ZLTextModel model = reader.read();
+		int emphasis = FB2Tag.EMPHASIS.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + emphasis +
+				"][TEXT]Тест 5[/TEXT][/CONTROL " + emphasis + "][/PARAGRAPH]\n");
+	}
 
+	public void testControlStrong() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test6.fb2");
+		ZLTextModel model = reader.read();
+		int strong = FB2Tag.STRONG.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + 
+				strong + "][TEXT]6[/TEXT][/CONTROL " + strong + "][/PARAGRAPH]\n");
+	}
+	
+	public void testControlStrikeThrough() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test7.fb2");
+		ZLTextModel model = reader.read();
+		int strikethrough = FB2Tag.STRIKETHROUGH.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + strikethrough +
+				"][TEXT]Тест 7[/TEXT][/CONTROL " + strikethrough + "][/PARAGRAPH]\n");
+	}
+	
+	public void testControlCode() {
+		FB2Reader reader = new FB2Reader("FB2ReaderTests/test8.fb2");
+		ZLTextModel model = reader.read();
+		int code = FB2Tag.CODE.ordinal();
+		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест 8. [/TEXT][CONTROL " + code +
+				"][TEXT]Оформление кода[/TEXT][/CONTROL " + code + "][/PARAGRAPH]\n");
+	}
 }
