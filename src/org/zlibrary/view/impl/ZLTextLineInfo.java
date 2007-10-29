2a6ce96b0ee214ac2852dbadc7d0c660d088d567
griffon <fbreader@fbreader.org>
2007-10-29 13:05:10 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@64 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/zlibrary/view/impl/ZLTextLineInfo.java
+++ src/org/zlibrary/view/impl/ZLTextLineInfo.java
@@ -1,11 +1,21 @@
 package org.zlibrary.view.impl;
 
 class ZLTextLineInfo {
-	ZLTextWordCursor myStart;
-	ZLTextWordCursor myRealStart;
-	ZLTextWordCursor myEnd;
-	boolean myIsVisible;
-	int myLeftIndent;
-	int myWidth;
-	int myHeight;
+	public ZLTextWordCursor myStart;
+	public ZLTextWordCursor myRealStart;
+	public ZLTextWordCursor myEnd;
+	public boolean myIsVisible;
+	public int myLeftIndent;
+	public int myWidth;
+	public int myHeight;
+
+	public ZLTextLineInfo(ZLTextWordCursor word) {
+		myStart = word;
+		myRealStart= word;
+		myEnd = word;
+		myIsVisible = false;
+		myLeftIndent = 0;
+		myWidth = 0;
+		myHeight = 0;
+	}
 }
--- src/org/zlibrary/view/impl/ZLTextViewImpl.java
+++ src/org/zlibrary/view/impl/ZLTextViewImpl.java
@@ -2,6 +2,10 @@ package org.zlibrary.view.impl;
 
 import org.zlibrary.view.ZLTextView;
 
-class ZLTextViewImpl /*implements ZLTextView*/ {
+class ZLTextViewImpl implements ZLTextView {
 
+	ZLTextLineInfo processTextLine(ZLTextWordCursor start, ZLTextWordCursor end) {
+		ZLTextLineInfo info = new ZLTextLineInfo(start);
+		return info;
+	}
 }
