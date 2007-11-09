76c49fb61a935b6d6eebbcbeb42b492c13919030
tushkanchik <fbreader@fbreader.org>
2007-11-09 09:36:54 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@94 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- test/org/test/fbreader/formats/fb2/TestFB2Reader.java
+++ test/org/test/fbreader/formats/fb2/TestFB2Reader.java
@@ -2,216 +2,215 @@ package org.test.fbreader.formats.fb2;
 
 import org.fbreader.bookmodel.BookModel;
 import org.fbreader.formats.fb2.FB2Reader;
-import org.fbreader.formats.fb2.FB2Tag;
 import org.zlibrary.text.model.ZLTextModel;
 import org.zlibrary.text.model.ZLTextParagraph;
 import org.zlibrary.text.model.entry.ZLTextEntry;
 
 import junit.framework.TestCase;
 
+import java.io.*;
+
 public class TestFB2Reader extends TestCase {
-	public void testOneParagraph() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test1.fb2");
+	
+	private boolean compareFiles(String f1, String f2) {
+		int i1;
+		int i2;
+		FileInputStream fis1 = null;
+		FileInputStream fis2 = null;
+		try {
+			fis1 = new FileInputStream(f1);
+			fis2 = new FileInputStream(f2);
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		try{
+			try{			
+				i1 = fis1.read();
+				i2 = fis2.read();
+				while ((i1 == i2) && (i1 != -1)){
+					i1 = fis1.read();
+					i2 = fis2.read();
+				}	
+			} finally {
+				fis1.close();
+				fis2.close();
+			}		
+		} catch (IOException e) {
+			return false;
+		}
+		return i1 == i2;
+	}
+	
+	private void writeDumpToFile(String inputFile, String outputFile) {
+		FB2Reader reader = new FB2Reader(inputFile);
 		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест 1[/TEXT][/PARAGRAPH]\n");
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(outputFile);
+			fos.write(model.dump().getBytes());
+			fos.close();			
+		} catch (Exception e) {
+			fail();
+		}		
+	}
+	
+	private void doTest(String test) {
+		String test_result = "test/Data/" + test + "_act.txt";
+		writeDumpToFile("test/Data/" + test + ".fb2", test_result);
+		assertTrue(compareFiles("test/Data/" + test + "_exp.txt", test_result));
+		(new File(test_result)).delete();
+	}
+	
+	public void testOneParagraph() {
+		doTest("test1");
 	}
 	
 	public void testTwoParagraphs() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test2.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(),
-				"[PARAGRAPH]\n[TEXT]Тест 2[/TEXT][/PARAGRAPH]\n[PARAGRAPH]\n[TEXT]Два абзаца[/TEXT][/PARAGRAPH]\n");
+		doTest("test2");
 	}
 	
 	public void testControlSup() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test3.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int sup = FB2Tag.SUP.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + sup +
-				"][TEXT]3[/TEXT][/CONTROL " + sup + "][/PARAGRAPH]\n");
+		doTest("test3");
 	}
 	
 	public void testControlSub() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test4.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int sub = FB2Tag.SUB.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + sub +
-				"][TEXT]4[/TEXT][/CONTROL " + sub + "][/PARAGRAPH]\n");
+		doTest("test4");
 	}
 	
 	public void testControlEmphasis() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test5.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int emphasis = FB2Tag.EMPHASIS.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + emphasis +
-				"][TEXT]Тест 5[/TEXT][/CONTROL " + emphasis + "][/PARAGRAPH]\n");
+		doTest("test5");
 	}
 
 	public void testControlStrong() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test6.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int strong = FB2Tag.STRONG.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест [/TEXT][CONTROL " + 
-				strong + "][TEXT]6[/TEXT][/CONTROL " + strong + "][/PARAGRAPH]\n");
+		doTest("test6");
 	}
 	
 	public void testControlStrikeThrough() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test7.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int strikethrough = FB2Tag.STRIKETHROUGH.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + strikethrough +
-				"][TEXT]Тест 7[/TEXT][/CONTROL " + strikethrough + "][/PARAGRAPH]\n");
+		doTest("test7");
 	}
 	
 	public void testControlCode() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/test8.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int code = FB2Tag.CODE.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Тест 8. [/TEXT][CONTROL " + code +
-				"][TEXT]Оформление кода[/TEXT][/CONTROL " + code + "][/PARAGRAPH]\n");
+		doTest("test8");
 	}
 	
 	public void testControlVerse() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/verse.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.V.ordinal() +
-				"][TEXT]Verse[/TEXT][/PARAGRAPH]\n");
+		doTest("verse");
 	}
 	
 	public void testControlDate() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/date.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.DATE.ordinal() +
-				"][TEXT]Date[/TEXT][/PARAGRAPH]\n");
+		doTest("date");
 	}
 	
 	public void testControlSubtitle() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/subtitle.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.SUBTITLE.ordinal() +
-				"][TEXT]Subtitle[/TEXT][/PARAGRAPH]\n");
+		doTest("subtitle");
 	}
 	
 	public void testControlTextAuthor() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/text_author.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.TEXT_AUTHOR.ordinal() +
-				"][TEXT]Text author[/TEXT][/PARAGRAPH]\n");
+		doTest("text_author");
 	}
 	
 	public void testEmptyLine() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/empty_line.fb2");
+		doTest("empty_line");
+	}
+	
+	public void testEmptyLineParagraphKind() {
+		FB2Reader reader = new FB2Reader("test/Data/empty_line.fb2");
 		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[/PARAGRAPH]\n");
 		assertEquals(model.getParagraph(0).getKind(),
 				ZLTextParagraph.Kind.EMPTY_LINE_PARAGRAPH);
 	}
 	
 	public void testControlCite() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/cite.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.CITE.ordinal() +
-				"][TEXT]Cite[/TEXT][/PARAGRAPH]\n");
+		doTest("cite");
 	}
 	
 	public void testControlEpigraph() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/epigraph.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + FB2Tag.EPIGRAPH.ordinal() +
-				"][TEXT]Epigraph[/TEXT][/PARAGRAPH]\n");
+		doTest("epigraph");
 	}
 	
 	public void testNotesSimple() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/simple_notes.fb2");
+		FB2Reader reader = new FB2Reader("test/Data/simple_notes.fb2");
 		assertTrue(reader.read().getFootnotes().containsKey("1"));
 		assertNotNull(reader.read().getFootnotes().get("1"));
 	}
 	
 	public void testOneNote() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/one_note.fb2");
+		FB2Reader reader = new FB2Reader("test/Data/one_note.fb2");
 		ZLTextModel model = reader.read().getFootnoteModel("1");
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]Note[/TEXT][/PARAGRAPH]\n");
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream("test/Data/one_note_act.txt");
+			fos.write(model.dump().getBytes());
+			fos.close();			
+		} catch (Exception e) {
+			fail();
+		}	
+		assertTrue(compareFiles("test/Data/one_note_exp.txt", "test/Data/one_note_act.txt"));
+		(new File("test/Data/one_note_act.txt")).delete();
 	}
 	
 	public void testStanza() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/stanza.fb2");
+		doTest("stanza");
+	}
+	
+	public void testStanzaParagraphKinds() {
+		FB2Reader reader = new FB2Reader("test/Data/stanza.fb2");
 		ZLTextModel model = reader.read().getBookModel();
 		assertEquals(model.getParagraph(0).getKind(), ZLTextParagraph.Kind.BEFORE_SKIP_PARAGRAPH);
 		assertEquals(model.getParagraph(2).getKind(), ZLTextParagraph.Kind.AFTER_SKIP_PARAGRAPH);
 		assertEquals(model.getParagraph(1).getKind(), ZLTextParagraph.Kind.TEXT_PARAGRAPH);
-		int stanza = FB2Tag.STANZA.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + stanza +
-				"][/PARAGRAPH]\n[PARAGRAPH]\n[CONTROL " + stanza + "][CONTROL " +
-				FB2Tag.V.ordinal() + "][TEXT]Stanza[/TEXT][/PARAGRAPH]\n[PARAGRAPH]\n[CONTROL " + 
-				stanza + "][/PARAGRAPH]\n");
 	}
 	
 	public void testAnnotation() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/annotation.fb2");
+		doTest("annotation");
+	}
+	
+	public void testAnnotationBeforeBodyParagraph() {
+		FB2Reader reader = new FB2Reader("test/Data/annotation_before.fb2");
 		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + 
-				FB2Tag.ANNOTATION.ordinal() + "][TEXT]annotation[/TEXT][/PARAGRAPH]\n");
+		assertEquals(model.getParagraph(1).getKind(),
+				ZLTextParagraph.Kind.END_OF_SECTION_PARAGRAPH);
 	}
 	
 	public void testAnnotationBeforeBody() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/annotation_before.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.getParagraph(1).getKind(),
-				ZLTextParagraph.Kind.END_OF_SECTION_PARAGRAPH);
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + 
-				FB2Tag.ANNOTATION.ordinal() +
-				"][TEXT]annotation[/TEXT][/PARAGRAPH]\n[PARAGRAPH]\n[/PARAGRAPH]\n");
+		doTest("annotation_before");
 	}
 	
 	public void testEndOfSection() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/section.fb2");
+		doTest("section");
+	}
+	
+	public void testEndOfSectionParagraph() {		
+		FB2Reader reader = new FB2Reader("test/Data/section.fb2");
 		ZLTextModel model = reader.read().getBookModel();
 		assertEquals(model.getParagraph(1).getKind(),
 				ZLTextParagraph.Kind.END_OF_SECTION_PARAGRAPH);
-		assertEquals(model.dump(), "[PARAGRAPH]\n[TEXT]section[/TEXT][/PARAGRAPH]\n[PARAGRAPH]\n[/PARAGRAPH]\n");
 	}
 	
 	public void testTitle() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/title.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + 
-				FB2Tag.TITLE.ordinal() + "][TEXT]Title[/TEXT][/PARAGRAPH]\n");
+		doTest("title");
 	}
 	
 	public void testPoemTitle() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/poem_title.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + 
-				FB2Tag.POEM.ordinal() + "][TEXT]Title[/TEXT][/PARAGRAPH]\n");
+		doTest("poem_title");
 	}
 	
 	public void testSectionTitle() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/section_title.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + 
-				FB2Tag.SECTION.ordinal() + "][TEXT]Title[/TEXT][/PARAGRAPH]\n");
+		doTest("section_title");
 	}
 	
 	public void testFootnote() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/footnote.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int footnote = FB2Tag.FOOTNOTE.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + footnote +
-				"][TEXT][1][/TEXT][/CONTROL " + footnote + "][/PARAGRAPH]\n");
+		doTest("footnote");
 	}
 	
 	public void testFootnote1() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/footnote1.fb2");
+		FB2Reader reader = new FB2Reader("test/Data/footnote1.fb2");
 		BookModel model = reader.read();
 		assertEquals("footnote", 
-				((ZLTextEntry)model.getParagraphByLink("note1").getEntries().get(0)).getData());
+				((ZLTextEntry)model.getParagraphByLink("note1").getEntries().get(1)).getData());
 	}
 	
 	public void testExternalHyperlink() {
-		FB2Reader reader = new FB2Reader("FB2ReaderTests/ext_hyperlink.fb2");
-		ZLTextModel model = reader.read().getBookModel();
-		int hyperlink = FB2Tag.A.ordinal();
-		assertEquals(model.dump(), "[PARAGRAPH]\n[CONTROL " + hyperlink +
-				"][TEXT][1][/TEXT][/CONTROL " + hyperlink + "][/PARAGRAPH]\n");
+		doTest("ext_hyperlink");
 	}
 }
