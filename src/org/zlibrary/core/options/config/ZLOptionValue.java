19521fc5c70548de362db67c427f7e06db348312
EugeniyVlasov <fbreader@fbreader.org>
2007-11-24 14:17:46 +0000

    то что я почемуто не выложил предыдущим коммитом
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@210 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ src/org/zlibrary/core/options/config/ZLOptionValue.java
@@ -0,0 +1,30 @@
+package org.zlibrary.core.options.config;
+
+public class ZLOptionValue {
+	private String myValue = "";
+	private String myCategory;
+	private String myName = "";
+	
+	public ZLOptionValue(String name, String value, String category) {
+		myValue = value;
+		myCategory = category;
+		myName = name;
+	}
+	
+	public void setValue(String value) {
+		myValue = value;
+	}
+	
+	public String getValue() {
+		return myValue;
+	}
+	
+	public String getCategory() {
+		return myCategory;
+	}
+	
+	public String toString() {
+		return "    <option name=\"" + myName 
+		+ "\" value=\"" + myValue + "\"/>\n";
+	}
+}
