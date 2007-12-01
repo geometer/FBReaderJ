3c1dff583af9d7155d73734351ef3c4e3a38db9d
Nikolay Pultsin <geometer@fbreader.org>
2007-12-01 16:10:26 +0000

    Base64 decoder call commented: it is not available for Android platform
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@270 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/fbreader/formats/fb2/Base64EncodedImage.java
+++ src/org/fbreader/formats/fb2/Base64EncodedImage.java
@@ -3,7 +3,7 @@ package org.fbreader.formats.fb2;
 //import org.apache.commons.codec.binary.Base64;
 import org.zlibrary.core.image.ZLImage;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+//import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class Base64EncodedImage implements ZLImage {
 	private String myEncodedData;
@@ -17,7 +17,7 @@ public class Base64EncodedImage implements ZLImage {
 		if ((myEncodedData == null) || (myData != null)) {
 			return;
 		}
-		myData = Base64.decode(myEncodedData);
+		//myData = Base64.decode(myEncodedData);
 		myEncodedData = null;
 	}
 	
