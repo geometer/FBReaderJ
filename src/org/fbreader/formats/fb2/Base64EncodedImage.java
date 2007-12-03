41cfc347295a1feb4f6107aa6622d7f70ad90dc2
tushkanchik <fbreader@fbreader.org>
2007-12-03 01:27:36 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@295 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/fbreader/formats/fb2/Base64EncodedImage.java
+++ src/org/fbreader/formats/fb2/Base64EncodedImage.java
@@ -3,8 +3,6 @@ package org.fbreader.formats.fb2;
 //import org.apache.commons.codec.binary.Base64;
 import org.zlibrary.core.image.ZLImage;
 
-//import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-
 public class Base64EncodedImage implements ZLImage {
 	private String myEncodedData;
 	private byte [] myData;
@@ -17,7 +15,47 @@ public class Base64EncodedImage implements ZLImage {
 		if ((myEncodedData == null) || (myData != null)) {
 			return;
 		}
-		//myData = Base64.decode(myEncodedData);
+		
+		int dataLength = myEncodedData.length();
+		int newLength = dataLength/4*3;
+		myData = new byte[newLength];
+		byte [] number = new byte[4];
+		for (int pos = 0, dataPos = 0; pos < dataLength; dataPos += 3) {
+			for (int i = 0; (i < 4) && (pos < dataLength); ++pos) {
+				char encodedByte = myEncodedData.charAt(pos);
+				number[i] = 0;
+				if (('A' <= encodedByte) && (encodedByte <= 'Z')) {
+					number[i] = (byte) (encodedByte - 'A');
+				} else if (('a' <= encodedByte) && (encodedByte <= 'z')) {
+					number[i]= (byte) (encodedByte - 'a' + 26);
+				} else if (('0' <= encodedByte) && (encodedByte <= '9')) {
+					number[i] = (byte) (encodedByte - '0' + 52);
+				} else if (encodedByte == '+') {
+					number[i] = 62;
+				} else if (encodedByte == '/') {
+					number[i] = 63;
+				} else if (encodedByte == '=') {
+					number[i] = 64;
+				} else {
+					continue;
+				}
+				++i;
+			}
+			myData[dataPos] = (byte) (number[0] <<2 | number[1] >>4 );
+			myData[dataPos + 1] = (byte) (((number[1] & 0xf)<<4 ) |( (number[2] >>2) & 0xf) );
+			myData[dataPos + 2] = (byte) ( number[2] <<6 | number[3]);
+		}
+/*		if (number[2] == 64) {
+			byte [] tmp = new byte[newLength - 2];
+			System.arraycopy(myData, 0, tmp, 0, newLength - 2);
+			myData = tmp;
+		} else if (number[3] == 64) {
+			byte [] tmp = new byte[newLength - 1];
+			System.arraycopy(myData, 0, tmp, 0, newLength - 1);
+			myData = tmp;
+		}
+			
+*/				
 		myEncodedData = null;
 	}
 	
