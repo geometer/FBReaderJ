d940d13e5c7c9831275beef97ddcfec2a2617715
Nikolay Pultsin <geometer@fbreader.org>
2007-12-12 05:03:51 +0000

    cleanup
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@381 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/zlibrary/text/model/entry/ZLTextControlEntry.java
+++ src/org/zlibrary/text/model/entry/ZLTextControlEntry.java
@@ -1,7 +1,7 @@
 package org.zlibrary.text.model.entry;
 
 public interface ZLTextControlEntry extends ZLTextParagraphEntry {
-    byte getKind();
-    boolean isStart();
-    boolean isHyperlink();
+	byte getKind();
+	boolean isStart();
+	boolean isHyperlink();
 }
