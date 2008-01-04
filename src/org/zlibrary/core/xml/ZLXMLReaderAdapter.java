681f42655ba6cf270aca6003da16d36b2c1869aa
Nikolay Pultsin <geometer@fbreader.org>
2008-01-04 15:45:36 +0000

    added missing file
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@436 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ src/org/zlibrary/core/xml/ZLXMLReaderAdapter.java
@@ -0,0 +1,30 @@
+package org.zlibrary.core.xml;
+
+public class ZLXMLReaderAdapter implements ZLXMLReader {
+	public boolean read(String fileName) {
+		return ZLXMLProcessorFactory.getInstance().createXMLProcessor().read(this, fileName);
+	}
+	
+	public boolean dontCacheAttributeValues() {
+		return false;
+	}
+
+	public void startElementHandler(String tag, StringMap attributes) {
+	}
+	
+	public void endElementHandler(String tag) {
+	}
+	
+	public void characterDataHandler(char[] ch, int start, int length) {
+	}
+
+	public void characterDataHandlerFinal(char[] ch, int start, int length) {
+		characterDataHandler(ch, start, length);	
+	}
+
+	public void startDocumentHandler() {
+	}
+	
+	public void endDocumentHandler() {
+	}
+}
