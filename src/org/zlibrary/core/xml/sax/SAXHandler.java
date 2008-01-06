4996a24399214449e2ff31fa3ebe1e78ed6c9298
Nikolay Pultsin <geometer@fbreader.org>
2008-01-06 14:18:19 +0000

    sax xml parser is compilable again
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@456 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/zlibrary/core/xml/sax/SAXHandler.java
+++ src/org/zlibrary/core/xml/sax/SAXHandler.java
@@ -5,30 +5,11 @@ import java.util.HashMap;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-import org.zlibrary.core.xml.ZLXMLReader;
+import org.zlibrary.core.xml.*;
 
 class SAXHandler extends DefaultHandler {	
 	private ZLXMLReader myXMLReader;
-	private static final class StringMap extends HashMap<String,String> implements ZLXMLReader.StringMap {
-		public int getSize() {
-			return size();
-		}
-
-		public String getKey(int index) {
-			int i = 0;
-			for (String s : keySet()) {
-				if (i++ == index) {
-					return s;
-				}
-			}
-			return null;
-		}
-
-		public String getValue(String key) {
-			return get(key);
-		}
-	}
-	private final StringMap myAttributes = new StringMap();
+	private final ZLStringMap myAttributes = new ZLStringMap();
 	
 	SAXHandler(ZLXMLReader reader) {
 		myXMLReader = reader;
@@ -39,13 +20,13 @@ class SAXHandler extends DefaultHandler {
 	}
 
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		StringMap attributesMap = myAttributes;
-		attributesMap.clear();
+		ZLStringMap attributesMap = myAttributes;
 		int length = attributes.getLength();
 		for (int i = 0; i < length; i++) {
 			attributesMap.put(attributes.getQName(i), attributes.getValue(i));
 		}
 		myXMLReader.startElementHandler(qName, attributesMap);
+		attributesMap.clear();
 	}
 
 	public void characters(char[] ch, int start, int length) throws SAXException {
@@ -58,9 +39,5 @@ class SAXHandler extends DefaultHandler {
 
 	public void startDocument() throws SAXException {
 		myXMLReader.startDocumentHandler();
-	}
-
-	public String getValue(String key) {
-		return myAttributes.get(key);
 	}
 }
