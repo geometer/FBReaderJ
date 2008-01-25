c416c814addf7341aa5acaead0e86bfea0666a0b
MarinaSokol <fbreader@fbreader.org>
2008-01-25 14:13:15 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@501 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/fbreader/description/Author.java
+++ src/org/fbreader/description/Author.java
@@ -90,12 +90,12 @@ public interface Author {
 		}
 	}
 
-	class AuthorComparator {
+	/*class AuthorComparator {
 		
 		public boolean operator(Author a1, Author a2) {
 			return a1.sortKey().equals(a2.sortKey());
 		}
-	}
+	}*/
 
 }
 
