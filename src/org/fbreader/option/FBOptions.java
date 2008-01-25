6e3d8e210775968097c20c2e90ab1fd2c8bfc806
MarinaSokol <fbreader@fbreader.org>
2008-01-25 09:49:53 +0000

    add fboptions
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@491 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ src/org/fbreader/option/FBOptions.java
@@ -0,0 +1,7 @@
+package org.fbreader.option;
+
+public class FBOptions {
+	public final static String BOOKS_CATEGORY = "books";
+	public final static String SEARCH_CATEGORY = "search";
+	public final static String EXTERNAL_CATEGORY = "external";
+}
