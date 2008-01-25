b7515784de371cb7dc81869b81b778f23b9d0637
MarinaSokol <fbreader@fbreader.org>
2008-01-25 12:49:32 +0000

    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@496 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ src/org/fbreader/description/BookDescription.java
@@ -0,0 +1,168 @@
+package org.fbreader.description;
+
+import java.util.Map;
+
+import org.fbreader.option.FBOptions;
+import org.zlibrary.core.filesystem.ZLFile;
+import org.zlibrary.core.options.ZLBooleanOption;
+import org.zlibrary.core.options.ZLIntegerRangeOption;
+import org.zlibrary.core.options.ZLStringOption;
+
+public class BookDescription {
+	private Author myAuthor;
+	private	String myTitle;
+	private	String mySequenceName;
+	private	int myNumberInSequence;
+	private	String myFileName;
+	private	String myLanguage;
+	private	String myEncoding;
+	private static Map<String, BookDescription> ourDescriptions;
+	
+	private static final String EMPTY = "";
+	private static final String UNKNOWN = "unknown";
+	
+//boolean checkFile = true
+	public static BookDescription getDescription(String fileName, boolean checkFile) {
+		String physicalFileName = new ZLFile(fileName).physicalFilePath();
+		ZLFile file = new ZLFile(physicalFileName);
+		if (checkFile && !file.exists()) {
+			return null;
+		}
+
+		BookDescription description = ourDescriptions.get(fileName);
+		if (description == null) {
+			description = new BookDescription(fileName);
+			ourDescriptions.put(fileName, description);
+		}
+
+		if (!checkFile || BookDescriptionUtil.checkInfo(file)) {
+			BookInfo info = new BookInfo(fileName);
+			description.myAuthor = SingleAuthor.create(info.AuthorDisplayNameOption.getValue(), info.AuthorSortKeyOption.getValue());
+			description.myTitle = info.TitleOption.getValue();
+			description.mySequenceName = info.SequenceNameOption.getValue();
+			description.myNumberInSequence = info.NumberInSequenceOption.getValue();
+			description.myLanguage = info.LanguageOption.getValue();
+			description.myEncoding = info.EncodingOption.getValue();
+			if (info.isFull()) {
+				return description;
+			}
+		} else {
+			if (physicalFileName != fileName) {
+				BookDescriptionUtil.resetZipInfo(file);
+			}
+			BookDescriptionUtil.saveInfo(file);
+		}
+
+		ZLFile bookFile = new ZLFile(fileName);
+		//FormatPlugin plugin = PluginCollection.instance().plugin(bookFile, false);
+		//if ((plugin == 0) || !plugin.readDescription(fileName, description)) {
+		//	return null;
+		//}
+
+		if (description.myTitle.equals("")) {
+			description.myTitle = ZLFile.fileNameToUtf8(bookFile.name(true));
+		}
+		Author author = description.myAuthor;
+		if (author == null || author.displayName().equals("")) {
+			description.myAuthor = SingleAuthor.create();
+		}
+		if (description.myEncoding.equals("")) {
+			description.myEncoding = "auto";
+		}
+		{
+			BookInfo info = new BookInfo(fileName);
+			info.AuthorDisplayNameOption.setValue(description.myAuthor.displayName());
+			info.AuthorSortKeyOption.setValue(description.myAuthor.sortKey());
+			info.TitleOption.setValue(description.myTitle);
+			info.SequenceNameOption.setValue(description.mySequenceName);
+			info.NumberInSequenceOption.setValue(description.myNumberInSequence);
+			info.LanguageOption.setValue(description.myLanguage);
+			info.EncodingOption.setValue(description.myEncoding);
+			info.IsSequenceDefinedOption.setValue(true);
+		}
+		return description;
+
+	}
+
+
+	private BookDescription(String fileName) {
+		myFileName = fileName;
+		myAuthor = null;
+		myNumberInSequence = 0;
+	}
+
+	public Author author() {
+		return myAuthor;
+	}
+	
+	public String title() {
+		return myTitle;
+	}
+	
+	public String sequenceName() {
+		return mySequenceName;
+	}
+	
+	public int numberInSequence() {
+		return myNumberInSequence; 
+	}
+	
+	public String fileName() {
+		return myFileName; 
+	}
+	
+	public String language() {
+		return myLanguage;
+	}
+	
+	public String encoding() {
+		return myEncoding;
+	}
+	
+	public static class BookInfo {
+		// This option is used to fix problem with missing sequence-related options
+		// in config in versions < 0.7.4k
+		// It makes no sense if old fbreader was never used on your device.
+		ZLBooleanOption IsSequenceDefinedOption;
+
+		public BookInfo(String fileName) {
+			AuthorDisplayNameOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "AuthorDisplayName", EMPTY);
+			AuthorSortKeyOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "AuthorSortKey", EMPTY);
+			TitleOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "Title", EMPTY);
+			SequenceNameOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "Sequence", EMPTY);
+			NumberInSequenceOption = new ZLIntegerRangeOption(FBOptions.BOOKS_CATEGORY, fileName, "Number in seq", 0, 100, 0);
+			LanguageOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "Language", UNKNOWN);
+			EncodingOption = new ZLStringOption(FBOptions.BOOKS_CATEGORY, fileName, "Encoding", EMPTY);
+			IsSequenceDefinedOption = new ZLBooleanOption(FBOptions.BOOKS_CATEGORY, fileName, "SequenceDefined", new ZLFile(fileName).extension().equals("fb2")); 
+			
+		}
+	
+		public boolean isFull() {
+			return
+			(!(AuthorDisplayNameOption.getValue().length() == 0) &&
+			!(AuthorSortKeyOption.getValue().length() == 0) &&
+			!(TitleOption.getValue().length() == 0) &&
+			!(EncodingOption.getValue().length() == 0) &&
+			IsSequenceDefinedOption.getValue());
+		}
+		
+		void reset() {
+			AuthorDisplayNameOption.setValue(EMPTY);
+			AuthorSortKeyOption.setValue(EMPTY);
+			TitleOption.setValue(EMPTY);
+			SequenceNameOption.setValue(EMPTY);
+			NumberInSequenceOption.setValue(0);
+			LanguageOption.setValue(UNKNOWN);
+			EncodingOption.setValue(EMPTY);
+		}
+
+		public ZLStringOption AuthorDisplayNameOption;
+		public ZLStringOption AuthorSortKeyOption;
+		public ZLStringOption TitleOption;
+		public ZLStringOption SequenceNameOption;
+		public ZLIntegerRangeOption NumberInSequenceOption;
+		public ZLStringOption LanguageOption;
+		public ZLStringOption EncodingOption;
+
+	}
+}
