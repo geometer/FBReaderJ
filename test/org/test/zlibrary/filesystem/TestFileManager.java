11daf851df22f89fcee2e156a744f9c845a26932
MarinaSokol <fbreader@fbreader.org>
2008-02-09 17:24:32 +0000

    add small tests
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@538 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- test/org/test/zlibrary/filesystem/TestFileManager.java
+++ test/org/test/zlibrary/filesystem/TestFileManager.java
@@ -3,34 +3,72 @@ package org.test.zlibrary.filesystem;
 
 import junit.framework.TestCase;
 
-import org.zlibrary.core.filesystem.ZLFSManager;
+import org.zlibrary.core.filesystem.ZLDir;
 import org.zlibrary.core.filesystem.ZLFile;
-import org.zlibrary.core.library.ZLibrary;
 import org.zlibrary.ui.swing.library.ZLSwingLibrary;
 
 public class TestFileManager extends TestCase {
+	private final String filename = "testfb2book.fb2";
+	private final String filenameZip = "testbookZip.zip";
+
+	private String myDirectory = "test\\data\\fb2\\filesystem";
+	
 	public void setUp() {
 		new ZLSwingLibrary().init();
 	}
 	
 	public void testCreateFile() {
-		ZLFile file = new ZLFile("test.fb2");
-    	file.getInputStream("test.fb2");
+		ZLFile file = new ZLFile(myDirectory + filename);
+    	file.getInputStream(myDirectory + filename);
 	}
 	
 	public void testCreateFile2() {
-		ZLFile file = new ZLFile("test.fb2");
-    	file.getInputStream("test.fb2");
-    	System.out.println(file.getDirectory());
-    	assertEquals(file.getExtension(), "fb2");
-       
-       	System.out.println(file.getPath());
-       	System.out.println(file.getPhysicalFilePath());
+		ZLFile file = new ZLFile(myDirectory + filename);
+    	file.getInputStream(myDirectory + filename);
+    	
+    	assertEquals(file.getDirectory(), null);
+    	assertEquals(file.getExtension(), "fb2");       
        	assertEquals(file.exists(), true);
-       	System.out.println(file.getName(true));
-       	System.out.println(file.getName(false));
-       	
+       	assertEquals(file.getName(true)+".fb2", file.getName(false));
+       	assertEquals(file.isArchive(), false);
+       	assertEquals(file.isArchive(), false);
+       	assertEquals(file.isDirectory(), false);
+       	assertEquals(file.isCompressed(), false);
 
 	}
+	
+	public void testCreateFile3() {
+		ZLFile file = new ZLFile(myDirectory + filenameZip);
+    	file.getInputStream(myDirectory + filenameZip);
+    	assertEquals(file.getDirectory(), null);
+    	assertEquals(file.getExtension(), "zip");       
+       	assertEquals(file.exists(), true);
+       	assertEquals(file.isArchive(), true);
+       	assertEquals(file.isDirectory(), false);
+       	assertEquals(file.isCompressed(), false);
+       	assertEquals(file.getName(true)+".zip", file.getName(false));
+	}
+	
+	public void testCreateFile4() {
+		ZLFile file = new ZLFile(myDirectory);
+    	file.getInputStream(myDirectory);
+    	ZLDir dir = file.getDirectory();
+    	//assertEquals(dir.getName(), myDirectory);
+        assertEquals(dir.getItemPath(".."), "test\\data\\fb2");
+        assertEquals(dir.getItemPath("marina"), "test\\data\\fb2\\filesystem\\marina");
+        assertEquals(dir.getParentPath(), "test\\data\\fb2");
+    	assertEquals(dir.getPath(), "test\\data\\fb2\\filesystem");
+    	assertEquals(dir.isRoot(), false);
+    	assertEquals(dir.getParentPath(), "test\\data\\fb2");
+        
+    	
+    	assertEquals(file.getExtension(), null);       
+       	assertEquals(file.exists(), true);
+       	assertEquals(file.isArchive(), false);
+       	assertEquals(file.isDirectory(), true);
+       	assertEquals(file.isCompressed(), false);
+       	assertEquals(file.getName(true), file.getName(false));
+	}
+
 
 }
