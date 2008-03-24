e181bc8ddcd727d53a53df5139688a0d3f8bca2c
Nikolay Pultsin <geometer@fbreader.org>
2008-03-24 06:21:24 +0000

    cleanup
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@711 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/fbreader/fbreader/FBIndicatorInfo.java
+++ src/org/fbreader/fbreader/FBIndicatorInfo.java
@@ -3,27 +3,26 @@ package org.fbreader.fbreader;
 import org.zlibrary.core.options.*;
 import org.zlibrary.core.util.ZLColor;
 import org.zlibrary.text.view.impl.ZLTextIndicatorInfo;
-import org.fbreader.option.FBOptions;
 
 class FBIndicatorInfo extends ZLTextIndicatorInfo {
 	private static final String INDICATOR = "Indicator";
 
 	public final ZLBooleanOption ShowOption =
-		new ZLBooleanOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Show", true);
+		new ZLBooleanOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Show", true);
 	public final ZLBooleanOption IsSensitiveOption =
-		new ZLBooleanOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "TouchSensitive", true);
+		new ZLBooleanOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "TouchSensitive", true);
 	public final ZLBooleanOption ShowTextPositionOption =
-		new ZLBooleanOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "PositionText", true);
+		new ZLBooleanOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "PositionText", true);
 	public final ZLBooleanOption ShowTimeOption =
-		new ZLBooleanOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Time", true);
+		new ZLBooleanOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Time", true);
 	public final ZLColorOption ColorOption =
-		new ZLColorOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Color", new ZLColor(127, 127, 127));
+		new ZLColorOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Color", new ZLColor(127, 127, 127));
 	public final ZLIntegerRangeOption HeightOption =
-		new ZLIntegerRangeOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Height", 1, 100, 16);
+		new ZLIntegerRangeOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Height", 1, 100, 16);
 	public final ZLIntegerRangeOption OffsetOption =
-		new ZLIntegerRangeOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Offset", 0, 100, 3);
+		new ZLIntegerRangeOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "Offset", 0, 100, 3);
 	public final ZLIntegerRangeOption FontSizeOption =
-		new ZLIntegerRangeOption(FBOptions.LOOK_AND_FEEL_CATEGORY, INDICATOR, "FontSize", 4, 72, 14);
+		new ZLIntegerRangeOption(ZLOption.LOOK_AND_FEEL_CATEGORY, INDICATOR, "FontSize", 4, 72, 14);
 
 	public boolean isVisible() {
 		return ShowOption.getValue();
--- src/org/fbreader/option/FBOptions.java
+++ src/org/fbreader/option/FBOptions.java
@@ -3,6 +3,5 @@ package org.fbreader.option;
 public class FBOptions {
 	public final static String BOOKS_CATEGORY = "books";
 	public final static String SEARCH_CATEGORY = "search";
-	public final static String LOOK_AND_FEEL_CATEGORY = "ui";
 	public final static String EXTERNAL_CATEGORY = "external";
 }
