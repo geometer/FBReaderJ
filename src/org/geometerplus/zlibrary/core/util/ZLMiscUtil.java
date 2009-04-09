af4a50f20cff110e0e9083c8d201d1e07dde6133
Nikolay Pultsin <geometer@fbreader.org>
2009-04-09 16:16:40 +0000

    separate class for a static equals method
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@917 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

---
+++ src/org/geometerplus/zlibrary/core/util/ZLMiscUtil.java
@@ -0,0 +1,7 @@
+package org.geometerplus.zlibrary.core.util;
+
+public abstract class ZLMiscUtil {
+	public static boolean equals(Object o0, Object o1) {
+		return (o0 == null) ? (o1 == null) : o0.equals(o1);
+	}
+}
