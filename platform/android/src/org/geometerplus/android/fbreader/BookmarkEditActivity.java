b34632152687fff84aa64710f2d8b0e55421608e
Nikolay Pultsin <geometer@fbreader.org>
2009-05-03 23:10:16 +0000

    more bookmark editing experiments
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@966 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- platform/android/src/org/geometerplus/android/fbreader/BookmarkEditActivity.java
+++ platform/android/src/org/geometerplus/android/fbreader/BookmarkEditActivity.java
@@ -21,17 +21,46 @@ package org.geometerplus.android.fbreader;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Window;
-import android.widget.LinearLayout;
-import android.widget.EditText;
+import android.view.*;
+import android.widget.*;
+import android.content.Context;
 
 import org.geometerplus.zlibrary.ui.android.R;
+
+class SimpleContainer extends ViewGroup {
+	private final View myChild;
+
+	SimpleContainer(Context context, View child) {
+		super(context);
+		myChild = child;
+		addView(child);
+	}
+
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		myChild.layout(left + 5, top + 5, right - 5, bottom - 5);
+	}
+}
 
 public class BookmarkEditActivity extends Activity {
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.edit_bookmark);
+		final LinearLayout v = new LinearLayout(this);
+		v.setOrientation(LinearLayout.VERTICAL);
+		v.addView(new EditText(this));
+		/*
+		final LinearLayout h = new LinearLayout(this);
+		v.addView(h);
+		final Button okButton = new Button(this);
+		okButton.setText("ok");
+		h.addView(okButton);
+		final Button cancelButton = new Button(this);
+		cancelButton.setText("cancel");
+		h.addView(cancelButton);
+		*/
+		final SimpleContainer container = new SimpleContainer(this, v);
+		//final SimpleContainer container = new SimpleContainer(this, new EditText(this));
+		setContentView(container);
 	}
 }
