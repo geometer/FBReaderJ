2aeaabf0542392187d19bcdd20d1857a00e1540e
Nikolay Pultsin <geometer@fbreader.org>
2009-10-09 01:35:58 +0000

    directories relayout
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@1037 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- createRawResources.py
+++ createRawResources.py
@@ -2,15 +2,13 @@
 
 import os, sys, shutil;
 
-project_dir = sys.argv[1]
-raw_res_dir = project_dir + "/res/raw"
-drawable_res_dir = project_dir + "/res/drawable"
-data_dir_common = project_dir + "/data"
-tree_icons_dir_android = project_dir + "/android_icons/tree"
-menu_icons_dir_android = project_dir + "/android_icons/menu"
-tabs_icons_dir_android = project_dir + "/android_icons/tabs"
-text_search_icons_dir_android = project_dir + "/android_icons/text_search"
-data_dir_android = project_dir + "/data"
+raw_res_dir = "res/raw"
+drawable_res_dir = "res/drawable"
+tree_icons_dir = "icons/tree"
+menu_icons_dir = "icons/menu"
+tabs_icons_dir = "icons/tabs"
+text_search_icons_dir = "icons/text_search"
+data_dir = "data"
 
 print os.getcwd()
 
@@ -34,10 +32,9 @@ def process_data_dir(prefix, dir, res_dir, replace_dot = 1):
 
 clean_res_dir(raw_res_dir)
 clean_res_dir(drawable_res_dir)
-process_data_dir("data__", data_dir_common, raw_res_dir)
-process_data_dir("data__", data_dir_android, raw_res_dir)
-shutil.copyfile(project_dir + "/android_icons/fbreader.png", drawable_res_dir + "/fbreader.png")
-process_data_dir("", tree_icons_dir_android, drawable_res_dir, 0)
-process_data_dir("", menu_icons_dir_android, drawable_res_dir, 0)
-process_data_dir("", tabs_icons_dir_android, drawable_res_dir, 0)
-process_data_dir("text_search_", text_search_icons_dir_android, drawable_res_dir, 0)
+process_data_dir("data__", data_dir, raw_res_dir)
+shutil.copyfile("icons/fbreader.png", drawable_res_dir + "/fbreader.png")
+process_data_dir("", tree_icons_dir, drawable_res_dir, 0)
+process_data_dir("", menu_icons_dir, drawable_res_dir, 0)
+process_data_dir("", tabs_icons_dir, drawable_res_dir, 0)
+process_data_dir("text_search_", text_search_icons_dir, drawable_res_dir, 0)
