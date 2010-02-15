bcc798d80119b3ec7077f955adb10666f1b66aaa
Nikolay Pultsin <geometer@fbreader.org>
2010-02-15 12:27:53 +0000

    updated copyright notices
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@1055 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/geometerplus/android/fbreader/ControlPanel.java
+++ src/org/geometerplus/android/fbreader/ControlPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Geometer Plus <contact@geometerplus.com>
+ * Copyright (C) 2009-2010 Geometer Plus <contact@geometerplus.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
