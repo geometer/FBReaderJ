fe682e12b82fec53b3bad4718e968323d8f256b9
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-04-30 13:20:18 +0000

    NetworkSearch service implementation
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/trunk@1325 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/geometerplus/android/fbreader/network/NetworkSearchActivity.java
+++ src/org/geometerplus/android/fbreader/network/NetworkSearchActivity.java
@@ -19,11 +19,20 @@
 
 package org.geometerplus.android.fbreader.network;
 
-import android.os.*;
-import android.app.*;
-import android.content.Intent;
+import java.util.List;
 
-import org.geometerplus.fbreader.network.NetworkLibrary;
+import android.app.AlertDialog;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.content.Intent;
+import android.net.Uri;
+
+import org.geometerplus.zlibrary.core.resources.ZLResource;
+
+import org.geometerplus.fbreader.network.*;
 
 
 public class NetworkSearchActivity extends Activity {
@@ -36,17 +45,106 @@ public class NetworkSearchActivity extends Activity {
 
 		final Intent intent = getIntent();
 		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	   		final String pattern = intent.getStringExtra(SearchManager.QUERY);
+			final String pattern = intent.getStringExtra(SearchManager.QUERY);
+			runSearch(pattern);
 		}
 		finish();
 	}
 
+	private class SearchHandler extends ItemsLoadingHandler {
 
-	protected boolean runSearch(final String pattern) {
+		private final SearchResult myResult;
+
+		public SearchHandler(SearchResult result) {
+			myResult = result;
+		}
+
+		public void onUpdateItems(List<NetworkLibraryItem> items) {
+			for (NetworkLibraryItem item: items) {
+				if (item instanceof NetworkBookItem) {
+					myResult.addBook((NetworkBookItem) item);
+				}
+			}
+		}
+
+		public void afterUpdateItems() {
+			final NetworkLibrary library = NetworkLibrary.Instance();
+			library.invalidate();
+			library.synchronize();
+			if (NetworkLibraryActivity.Instance != null) {
+				NetworkLibraryActivity.Instance.resetTree();
+			}
+		}
+
+		public void onFinish(String errorMessage) {
+			afterUpdateCatalog(errorMessage, myResult.empty());
+		}
+
+		private void afterUpdateCatalog(String errorMessage, boolean childrenEmpty) {
+			final ZLResource dialogResource = ZLResource.resource("dialog");
+			ZLResource boxResource = null;
+			String msg = null;
+			if (errorMessage != null) {
+				boxResource = dialogResource.getResource("networkError");
+				msg = errorMessage;
+			} else if (childrenEmpty) {
+				boxResource = dialogResource.getResource("emptySearchResults");
+				msg = boxResource.getResource("message").getValue();
+			}
+			if (msg != null) {
+				if (NetworkLibraryActivity.Instance != null) {
+					final ZLResource buttonResource = dialogResource.getResource("button");
+					new AlertDialog.Builder(NetworkLibraryActivity.Instance)
+						.setTitle(boxResource.getResource("title").getValue())
+						.setMessage(msg)
+						.setIcon(0)
+						.setPositiveButton(buttonResource.getResource("ok").getValue(), null)
+						.create().show();
+				}
+				// TODO: else show notification???
+			}
+		}
+	}
+
+	private static class SearchRunnable extends ItemsLoadingRunnable {
+
+		private final String myPattern;
+
+		public SearchRunnable(ItemsLoadingHandler handler, String pattern) {
+			super(handler);
+			myPattern = pattern;
+		}
+
+		public String doBefore() {
+			return null;
+		}
+
+		public String doLoading(NetworkOperationData.OnNewItemListener doWithListener) {
+			return NetworkLibrary.Instance().simpleSearch(myPattern, doWithListener);
+		}
+	}
+
+	protected void runSearch(final String pattern) {
 		final NetworkLibrary library = NetworkLibrary.Instance();
 		library.NetworkSearchPatternOption.setValue(pattern);
-		// run search
-		return true; // return error status
+
+		if (NetworkLibraryActivity.Instance == null) {
+			return;
+		}
+
+		final String summary = ZLResource.resource("networkView").getResource("searchResults").getValue().replace("%s", pattern);
+
+		final SearchResult result = new SearchResult(summary);
+
+		library.setSearchResult(result);
+		library.invalidate();
+		library.synchronize();
+
+		final SearchHandler handler = new SearchHandler(result);
+		NetworkLibraryActivity.Instance.startItemsLoading(
+			"org.geometerplus.android.fbreader.network.NetworkSearchActivity",
+			new SearchRunnable(handler, pattern)
+		);
 	}
 
 	protected Activity getParentActivity() {
