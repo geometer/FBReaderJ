0c30c05e8c8905e99ab3fbf86de773748455e086
Yuri Chernyavskiy <y.chernyavskiy@gmail.com>
2010-10-01 02:55:59 +0400

    footer: tap and flip scrolling are enabled together
    In FBView scrolling start by flip is moved to onStylusMovePressed.
    In ZLAndroidWidget during first move event additional view.onStylusPress
    is called with pressed coords.

--- src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
+++ src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
@@ -374,7 +374,7 @@ public class ZLAndroidWidget extends View {
 							if (myPendingLongClickRunnable != null) {
 								removeCallbacks(myPendingLongClickRunnable);
 							}
-							view.onStylusPress(myPressedX, myPressedY);
+							view.onStylusMovePressed(myPressedX, myPressedY);
 							myPendingPress = false;
 						}
 					}
