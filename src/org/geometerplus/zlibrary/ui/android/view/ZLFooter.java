760f3aff4c647cd5d99f4016e904f2159851f5b4
Nikolay Pultsin <geometer@fbreader.org>
2010-10-05 03:33:14 +0100

    footer: copyright has been added to ZLFooter
    - ZLFooter.java: Copyright has been added to ZLFooter.
    - ZLFooter.java: Imports have been reordered.

--- src/org/geometerplus/zlibrary/ui/android/view/ZLFooter.java
+++ src/org/geometerplus/zlibrary/ui/android/view/ZLFooter.java
@@ -1,13 +1,25 @@
+/*
+ * Copyright (C) 2010 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 package org.geometerplus.zlibrary.ui.android.view;
 
 import java.util.Date;
-
-import org.geometerplus.fbreader.bookmodel.FBHyperlinkType;
-import org.geometerplus.fbreader.fbreader.FBReader;
-import org.geometerplus.zlibrary.core.application.ZLApplication;
-import org.geometerplus.zlibrary.core.view.ZLView;
-import org.geometerplus.zlibrary.ui.android.library.ZLAndroidApplication;
-import org.geometerplus.zlibrary.ui.android.util.ZLAndroidColorUtil;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -15,6 +27,15 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+
+import org.geometerplus.zlibrary.core.application.ZLApplication;
+import org.geometerplus.zlibrary.core.view.ZLView;
+
+import org.geometerplus.zlibrary.ui.android.library.ZLAndroidApplication;
+import org.geometerplus.zlibrary.ui.android.util.ZLAndroidColorUtil;
+
+import org.geometerplus.fbreader.bookmodel.FBHyperlinkType;
+import org.geometerplus.fbreader.fbreader.FBReader;
 
 public class ZLFooter {
 	private final int SIZE_SMALL = 0;
