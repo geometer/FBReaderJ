2f4635f9448ff2cf0c2569cac69bc2580795fc47
Nikolay Pultsin <geometer@fbreader.org>
2010-10-05 23:38:31 +0100

    Power consumption has been fixed

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -168,6 +168,7 @@ public final class FBReader extends ZLAndroidActivity {
 	public void onPause() {
 		if (myWakeLock != null) {
 			myWakeLock.release();
+			myWakeLock = null;
 		}
 		ControlButtonPanel.saveVisibilities();
 		super.onPause();
