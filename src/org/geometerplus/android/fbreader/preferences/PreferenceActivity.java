a46b15b0559db65a1dc00d3c9bd715516d193c24
Nikolay Pultsin <geometer@fbreader.org>
2010-10-24 10:34:07 +0100

    footer long tap option is temporary commented

--- src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
@@ -101,10 +101,12 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 			fbReader.FooterHeightOption)
 		);
 
+		/*
 		String[] footerLongTaps = {"longTapRevert", "longTapNavigate"};
 		statusLineCategory.addPreference(new ZLChoicePreference(
 			this, statusLineCategory.Resource, "footerLongTap",
 			fbReader.FooterLongTap, footerLongTaps));
+		*/
 
 		statusLineCategory.addOption(fbReader.FooterShowClock, "showClock");
 		statusLineCategory.addOption(fbReader.FooterShowBattery, "showBattery");
