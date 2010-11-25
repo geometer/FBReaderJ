416dbb45a34913696b83ae6bd1078440a39164c8
Nikolay Pultsin <geometer@fbreader.org>
2010-11-25 22:24:11 +0000

    TRANSLATE action has been added

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -117,6 +117,8 @@ public final class FBReader extends ZLAndroidActivity {
 		
 		fbReader.addAction(ActionCode.SHOW_NAVIGATION, new ShowNavigationAction(this, fbReader));
 		fbReader.addAction(ActionCode.SEARCH, new SearchAction(this, fbReader));
+
+		fbReader.addAction(ActionCode.TRANSLATE, new TranslateAction(this, fbReader));
 	}
 
 	@Override
--- src/org/geometerplus/fbreader/fbreader/ActionCode.java
+++ src/org/geometerplus/fbreader/fbreader/ActionCode.java
@@ -47,8 +47,10 @@ public interface ActionCode {
 	String TOGGLE_FULLSCREEN = "toggleFullscreen";
 	String FULLSCREEN_ON = "onFullscreen";
 	String QUIT = "quit";
+
 	String COPY_SELECTED_TEXT_TO_CLIPBOARD = "copyToClipboard";
 	String CLEAR_SELECTION = "clearSelection";
-	String OPEN_SELECTED_TEXT_IN_DICTIONARY = "openInDictionary";
+	String TRANSLATE = "translate";
+
 	String FOLLOW_HYPERLINK = "followHyperlink";
 };
