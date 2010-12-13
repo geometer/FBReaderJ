378ecbe12bbc065d04d35d902744cb4f2e7ff406
Nikolay Pultsin <geometer@fbreader.org>
2010-12-13 15:22:20 +0300

    cleanup: copyright notice, code style

--- src/org/geometerplus/android/fbreader/library/FManagerAdapter.java
+++ src/org/geometerplus/android/fbreader/library/FManagerAdapter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 package org.geometerplus.android.fbreader.library;
 
 import java.util.List;
@@ -66,9 +85,9 @@ public class FManagerAdapter extends ArrayAdapter<FileOrder>{
 			coverView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
 			coverView.requestLayout();
 			
-			if(order.getBook() == null)
+			if (order.getBook() == null) {
 				coverView.setImageResource(R.drawable.ic_list_library_folder);
-			else{
+			} else {
 				Log.v(FileManager.LOG, "isBook");
 				Book book = order.getBook();
 				FormatPlugin plugin = PluginCollection.Instance().getPlugin(book.File);
