b47b0bbeeb0ca29e67befe8d134ee8925b6a71ca
Nikolay Pultsin <geometer@fbreader.org>
2010-12-13 00:57:53 +0000

    compilation issue has been fixed

--- src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
+++ src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
@@ -464,8 +464,6 @@ public class ZLAndroidWidget extends View {
 			case KeyEvent.KEYCODE_VOLUME_UP:
 			case KeyEvent.KEYCODE_BACK:
 			case KeyEvent.KEYCODE_DPAD_CENTER:
-			case KeyEvent.KEYCODE_DPAD_LEFT:
-			case KeyEvent.KEYCODE_DPAD_RIGHT:
 			case KeyEvent.KEYCODE_DPAD_DOWN:
 			case KeyEvent.KEYCODE_DPAD_UP:
 			case KeyEvent.KEYCODE_DPAD_RIGHT:
