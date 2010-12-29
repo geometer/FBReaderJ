ad9da693894318f52085b4ce8d713e5a22c5097c
Nikolay Pultsin <geometer@fbreader.org>
2010-12-29 20:46:32 +0000

    KillerCallback instead of KillerService

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -44,7 +44,7 @@ import org.geometerplus.zlibrary.ui.android.library.ZLAndroidApplication;
 import org.geometerplus.fbreader.fbreader.ActionCode;
 import org.geometerplus.fbreader.fbreader.FBReaderApp;
 
-import org.geometerplus.android.fbreader.library.KillerService;
+import org.geometerplus.android.fbreader.library.KillerCallback;
 
 public final class FBReader extends ZLAndroidActivity {
 	public static final String BOOK_PATH_KEY = "BookPath";
@@ -185,7 +185,7 @@ public final class FBReader extends ZLAndroidActivity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		startService(new Intent(getApplicationContext(), KillerService.class));
+		sendBroadcast(new Intent(getApplicationContext(), KillerCallback.class));
 		ControlButtonPanel.restoreVisibilities();
 	}
 
