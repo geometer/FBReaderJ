8be5c6e0fd13c651ef9a5da7be4783443f9a9a1e
Nikolay Pultsin <geometer@fbreader.org>
2010-12-30 22:31:34 +0000

    InvalidStateException has been fixed(?)

--- src/org/geometerplus/android/fbreader/library/FileManager.java
+++ src/org/geometerplus/android/fbreader/library/FileManager.java
@@ -312,17 +312,16 @@ public final class FileManager extends BaseActivity {
 
 			final ArrayList<ZLFile> children = new ArrayList<ZLFile>(myFile.children());
 			Collections.sort(children, new FileComparator());
-			for (ZLFile file : children) {
+			for (final ZLFile file : children) {
 				if (Thread.currentThread().isInterrupted()) {
 					break;
 				}
 				if (file.isDirectory() || file.isArchive() ||
 					PluginCollection.Instance().getPlugin(file) != null) {
-					final FileListAdapter adapter = (FileListAdapter)getListAdapter();
-					adapter.add(new FileItem(file));
-//					adapter.notifyDataSetChanged();	// TODO question!
 					runOnUiThread(new Runnable() {
 						public void run() {
+							final FileListAdapter adapter = (FileListAdapter)getListAdapter();
+							adapter.add(new FileItem(file));
 							adapter.notifyDataSetChanged();
 						}
 					});
