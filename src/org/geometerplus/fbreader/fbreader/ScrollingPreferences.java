45b1961270fb3912227cab6c17aa2e760c8e6390
Nikolay Pultsin <geometer@fbreader.org>
2011-01-22 20:58:00 +0000

    tap zones scheme option

--- src/org/geometerplus/fbreader/fbreader/ScrollingPreferences.java
+++ src/org/geometerplus/fbreader/fbreader/ScrollingPreferences.java
@@ -38,14 +38,19 @@ public class ScrollingPreferences {
 
 	public final ZLBooleanOption VolumeKeysOption =
 		new ZLBooleanOption("Scrolling", "VolumeKeys", true);
+	public final ZLBooleanOption InvertVolumeKeysOption =
+		new ZLBooleanOption("Scrolling", "InvertVolumeKeys", false);
 
 	public final ZLEnumOption<ZLView.Animation> AnimationOption =
 		new ZLEnumOption<ZLView.Animation>("Scrolling", "Animation", ZLView.Animation.slide);
 
 	public final ZLBooleanOption HorizontalOption =
 		new ZLBooleanOption("Scrolling", "Horizontal", true);
-	public final ZLBooleanOption InvertVolumeKeysOption =
-		new ZLBooleanOption("Scrolling", "InvertVolumeKeys", false);
+	public enum TapZoneScheme {
+		left_to_right, right_to_left, up, down, custom
+	}
+	public final ZLEnumOption<TapZoneScheme> TapZoneSchemeOption =
+		new ZLEnumOption<TapZoneScheme>("Scrolling", "TapZoneScheme", TapZoneScheme.right_to_left);
 
 	private ScrollingPreferences() {
 		ourInstance = this;
