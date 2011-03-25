73a8fe55e5e4fbc6f56a47eb81baf8e284d36210
Nikolay Pultsin <geometer@fbreader.org>
2011-03-25 01:59:04 +0000

    cleanup

--- src/org/geometerplus/android/fbreader/ProcessHyperlinkAction.java
+++ src/org/geometerplus/android/fbreader/ProcessHyperlinkAction.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.ActivityNotFoundException;
 import android.net.Uri;
 
-import org.geometerplus.zlibrary.core.resources.ZLResource;
 import org.geometerplus.zlibrary.core.network.ZLNetworkException;
 
 import org.geometerplus.zlibrary.text.view.*;
