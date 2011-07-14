15f43e0a3fe28b3186a2fc66c2e9667b85836123
Nikolay Pultsin <geometer@fbreader.org>
2011-07-14 18:56:53 +0100

    library-dependent code has been moved from BaseActivity to LibraryBaseActivity

--- src/org/geometerplus/android/fbreader/library/BaseActivity.java
+++ src/org/geometerplus/android/fbreader/library/BaseActivity.java
@@ -34,10 +34,10 @@ import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
+import org.geometerplus.android.util.UIUtil;
+
 import org.geometerplus.fbreader.library.*;
 import org.geometerplus.fbreader.tree.FBTree;
-
-import org.geometerplus.android.util.UIUtil;
 
 import org.geometerplus.android.fbreader.FBReader;
 import org.geometerplus.android.fbreader.BookInfoActivity;
@@ -104,41 +104,7 @@ abstract class BaseActivity extends ListActivity implements View.OnCreateContext
 		return (ListAdapter)super.getListAdapter();
 	}
 
-	int getCoverResourceId(FBTree tree) {
-		if (((LibraryTree)tree).getBook() != null) {
-			return R.drawable.ic_list_library_book;
-		} else if (tree instanceof FirstLevelTree) {
-			final String id = tree.getUniqueKey().Id;
-			if (Library.ROOT_FAVORITES.equals(id)) {
-				return R.drawable.ic_list_library_favorites;
-			} else if (Library.ROOT_RECENT.equals(id)) {
-				return R.drawable.ic_list_library_recent;
-			} else if (Library.ROOT_BY_AUTHOR.equals(id)) {
-				return R.drawable.ic_list_library_authors;
-			} else if (Library.ROOT_BY_TITLE.equals(id)) {
-				return R.drawable.ic_list_library_books;
-			} else if (Library.ROOT_BY_TAG.equals(id)) {
-				return R.drawable.ic_list_library_tags;
-			} else if (Library.ROOT_FILE_TREE.equals(id)) {
-				return R.drawable.ic_list_library_folder;
-			}
-		} else if (tree instanceof FileTree) {
-			final ZLFile file = ((FileTree)tree).getFile();
-			if (file.isArchive()) {
-				return R.drawable.ic_list_library_zip;
-			} else if (file.isDirectory() && file.isReadable()) {
-				return R.drawable.ic_list_library_folder;
-			} else {
-				return R.drawable.ic_list_library_permission_denied;
-			}
-		} else if (tree instanceof AuthorTree) {
-			return R.drawable.ic_list_library_author;
-		} else if (tree instanceof TagTree) {
-			return R.drawable.ic_list_library_tag;
-		}
-
-		return R.drawable.ic_list_library_books;
-	}
+	protected abstract int getCoverResourceId(FBTree tree);
 
 	@Override
 	protected void onListItemClick(ListView listView, View view, int position, long rowId) {
--- src/org/geometerplus/android/fbreader/library/LibraryBaseActivity.java
+++ src/org/geometerplus/android/fbreader/library/LibraryBaseActivity.java
@@ -25,12 +25,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import org.geometerplus.zlibrary.core.options.ZLStringOption;
+import org.geometerplus.zlibrary.core.filesystem.ZLFile;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
 import org.geometerplus.android.util.UIUtil;
 
-import org.geometerplus.fbreader.library.Library;
+import org.geometerplus.fbreader.library.*;
+import org.geometerplus.fbreader.tree.FBTree;
 
 abstract class LibraryBaseActivity extends BaseActivity implements MenuItem.OnMenuItemClickListener {
 	static final ZLStringOption BookSearchPatternOption =
@@ -88,4 +90,41 @@ abstract class LibraryBaseActivity extends BaseActivity implements MenuItem.OnMe
                 return true;
         }
     }
+
+	@Override
+	protected int getCoverResourceId(FBTree tree) {
+		if (((LibraryTree)tree).getBook() != null) {
+			return R.drawable.ic_list_library_book;
+		} else if (tree instanceof FirstLevelTree) {
+			final String id = tree.getUniqueKey().Id;
+			if (Library.ROOT_FAVORITES.equals(id)) {
+				return R.drawable.ic_list_library_favorites;
+			} else if (Library.ROOT_RECENT.equals(id)) {
+				return R.drawable.ic_list_library_recent;
+			} else if (Library.ROOT_BY_AUTHOR.equals(id)) {
+				return R.drawable.ic_list_library_authors;
+			} else if (Library.ROOT_BY_TITLE.equals(id)) {
+				return R.drawable.ic_list_library_books;
+			} else if (Library.ROOT_BY_TAG.equals(id)) {
+				return R.drawable.ic_list_library_tags;
+			} else if (Library.ROOT_FILE_TREE.equals(id)) {
+				return R.drawable.ic_list_library_folder;
+			}
+		} else if (tree instanceof FileTree) {
+			final ZLFile file = ((FileTree)tree).getFile();
+			if (file.isArchive()) {
+				return R.drawable.ic_list_library_zip;
+			} else if (file.isDirectory() && file.isReadable()) {
+				return R.drawable.ic_list_library_folder;
+			} else {
+				return R.drawable.ic_list_library_permission_denied;
+			}
+		} else if (tree instanceof AuthorTree) {
+			return R.drawable.ic_list_library_author;
+		} else if (tree instanceof TagTree) {
+			return R.drawable.ic_list_library_tag;
+		}
+
+		return R.drawable.ic_list_library_books;
+	}
 }
