d37054f9e99eb042f06dc7b44e6522ccf37f2b50
Nikolay Pultsin <geometer@fbreader.org>
2011-07-14 22:48:51 +0100

    fixed compilation issue

--- src/org/geometerplus/android/fbreader/library/BaseActivity.java
+++ src/org/geometerplus/android/fbreader/library/BaseActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 
 import org.geometerplus.android.util.UIUtil;
 
