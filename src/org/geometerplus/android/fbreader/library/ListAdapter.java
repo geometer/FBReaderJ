946bffd7ac2182c787cad3a82fc2b9c8a629ca4b
Nikolay Pultsin <geometer@fbreader.org>
2011-07-14 01:01:49 +0100

    code cleanup

--- src/org/geometerplus/android/fbreader/library/ListAdapter.java
+++ src/org/geometerplus/android/fbreader/library/ListAdapter.java
@@ -184,21 +184,20 @@ public class ListAdapter extends BaseAdapter implements View.OnCreateContextMenu
 		}
 
 		final ImageView coverView = getCoverView(view);
-
-			final Bitmap coverBitmap = getCoverBitmap(tree.getCover());
-			if (coverBitmap != null) {
-				coverView.setImageBitmap(coverBitmap);
-			} else if (tree instanceof ZLAndroidTree) {
-				coverView.setImageResource(((ZLAndroidTree)tree).getCoverResourceId());
-			} else if (tree instanceof AuthorTree) {
-				coverView.setImageResource(R.drawable.ic_list_library_author);
-			} else if (tree instanceof TagTree) {
-				coverView.setImageResource(R.drawable.ic_list_library_tag);
-			} else if (tree instanceof BookTree) {
-				coverView.setImageResource(R.drawable.ic_list_library_book);
-			} else {
-				coverView.setImageResource(R.drawable.ic_list_library_books);
-			}
+		final Bitmap coverBitmap = getCoverBitmap(tree.getCover());
+		if (coverBitmap != null) {
+			coverView.setImageBitmap(coverBitmap);
+		} else if (tree instanceof ZLAndroidTree) {
+			coverView.setImageResource(((ZLAndroidTree)tree).getCoverResourceId());
+		} else if (tree instanceof AuthorTree) {
+			coverView.setImageResource(R.drawable.ic_list_library_author);
+		} else if (tree instanceof TagTree) {
+			coverView.setImageResource(R.drawable.ic_list_library_tag);
+		} else if (tree instanceof BookTree) {
+			coverView.setImageResource(R.drawable.ic_list_library_book);
+		} else {
+			coverView.setImageResource(R.drawable.ic_list_library_books);
+		}
 
 		return view;
 	}
