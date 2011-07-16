2186b68e7be2aa146f471db0f2e49aff4c6e4398
Nikolay Pultsin <geometer@fbreader.org>
2011-07-16 23:19:19 +0100

    navigation history in BaseActivity

--- src/org/geometerplus/android/fbreader/tree/BaseActivity.java
+++ src/org/geometerplus/android/fbreader/tree/BaseActivity.java
@@ -19,6 +19,8 @@
 
 package org.geometerplus.android.fbreader.tree;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -34,8 +36,13 @@ public abstract class BaseActivity extends ListActivity {
 
 	public static final String TREE_KEY_KEY = "TreeKey";
 	public static final String SELECTED_TREE_KEY_KEY = "SelectedTreeKey";
+	public static final String HISTORY_KEY = "HistoryKey";
 
 	private FBTree myCurrentTree;
+	// we store the key separately because
+	// it will be changed after myCurrentTree.removeSelf() call
+	private FBTree.Key myCurrentKey;
+	private ArrayList<FBTree.Key> myHistory;
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -52,6 +59,16 @@ public abstract class BaseActivity extends ListActivity {
 		return myCurrentTree;
 	}
 
+	@Override
+	protected void onNewIntent(Intent intent) {
+		if (OPEN_TREE_ACTION.equals(intent.getAction())) {
+			init(intent);
+		} else {
+			super.onNewIntent(intent);
+		}
+	}
+
+	protected abstract FBTree getTreeByKey(FBTree.Key key);
 	public abstract boolean isTreeSelected(FBTree tree);
 
 	protected boolean OLD_STYLE_FLAG = false;
@@ -62,19 +79,28 @@ public abstract class BaseActivity extends ListActivity {
 			return super.onKeyDown(keyCode, event);
 		}
 
-		if (keyCode == KeyEvent.KEYCODE_BACK && myCurrentTree.Parent != null) {
-			openTree(myCurrentTree.Parent, myCurrentTree);
-			return true;
-		} else {
-			return super.onKeyDown(keyCode, event);
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			FBTree parent = null;
+			while (parent == null && !myHistory.isEmpty()) {
+				parent = getTreeByKey(myHistory.remove(myHistory.size() - 1));
+			}
+			if (parent == null) {
+				parent = myCurrentTree.Parent;
+			}
+			if (parent != null) {
+				openTree(parent, myCurrentTree, false);
+				return true;
+			}
 		}
+
+		return super.onKeyDown(keyCode, event);
 	}
 
 	protected void openTree(final FBTree tree) {
-		openTree(tree, null);
+		openTree(tree, null, true);
 	}
 
-	protected void openTree(final FBTree tree, final FBTree treeToSelect) {
+	protected void openTree(final FBTree tree, final FBTree treeToSelect, final boolean storeInHistory) {
 		switch (tree.getOpeningStatus()) {
 			case WAIT_FOR_OPEN:
 			case ALWAYS_RELOAD_BEFORE_OPENING:
@@ -89,29 +115,18 @@ public abstract class BaseActivity extends ListActivity {
 						},
 						new Runnable() {
 							public void run() {
-								openTreeInternal(tree, treeToSelect);
+								openTreeInternal(tree, treeToSelect, storeInHistory);
 							}
 						}
 					);
 				} else {
 					tree.waitForOpening();
-					openTreeInternal(tree, treeToSelect);
+					openTreeInternal(tree, treeToSelect, storeInHistory);
 				}
 				break;
 			default:
-				openTreeInternal(tree, treeToSelect);
+				openTreeInternal(tree, treeToSelect, storeInHistory);
 				break;
-		}
-	}
-
-	protected abstract FBTree getTreeByKey(FBTree.Key key);
-
-	@Override
-	protected void onNewIntent(Intent intent) {
-		if (OPEN_TREE_ACTION.equals(intent.getAction())) {
-			init(intent);
-		} else {
-			super.onNewIntent(intent);
 		}
 	}
 
@@ -119,18 +134,29 @@ public abstract class BaseActivity extends ListActivity {
 		final FBTree.Key key = (FBTree.Key)intent.getSerializableExtra(TREE_KEY_KEY);
 		final FBTree.Key selectedKey = (FBTree.Key)intent.getSerializableExtra(SELECTED_TREE_KEY_KEY);
 		myCurrentTree = getTreeByKey(key);
+		// not myCurrentKey = key
+		// because key might be null
+		myCurrentKey = myCurrentTree.getUniqueKey();
 		final ListAdapter adapter = getListAdapter();
 		adapter.replaceAll(myCurrentTree.subTrees());
 		setTitle(myCurrentTree.getTreeTitle());
 		final FBTree selectedTree =
 			selectedKey != null ? getTreeByKey(selectedKey) : adapter.getFirstSelectedItem();
 		setSelection(adapter.getIndex(selectedTree));
+
+		myHistory = (ArrayList<FBTree.Key>)intent.getSerializableExtra(HISTORY_KEY);
+		if (myHistory == null) {
+			myHistory = new ArrayList<FBTree.Key>();
+		}
 	}
 
-	private void openTreeInternal(FBTree tree, FBTree treeToSelect) {
+	private void openTreeInternal(FBTree tree, FBTree treeToSelect, boolean storeInHistory) {
 		switch (tree.getOpeningStatus()) {
 			case READY_TO_OPEN:
 			case ALWAYS_RELOAD_BEFORE_OPENING:
+				if (storeInHistory && !myCurrentKey.equals(tree.getUniqueKey())) {
+					myHistory.add(myCurrentKey);
+				}
 				startActivity(new Intent(this, getClass())
 					.setAction(OPEN_TREE_ACTION)
 					.putExtra(TREE_KEY_KEY, tree.getUniqueKey())
@@ -138,6 +164,7 @@ public abstract class BaseActivity extends ListActivity {
 						SELECTED_TREE_KEY_KEY,
 						treeToSelect != null ? treeToSelect.getUniqueKey() : null
 					)
+					.putExtra(HISTORY_KEY, myHistory)
 				);
 				break;
 			case CANNOT_OPEN:
