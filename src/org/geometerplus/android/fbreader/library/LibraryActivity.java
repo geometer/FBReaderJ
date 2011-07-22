90411abb79a2d6ee3ef25ec2d21a1228910f56aa
Nikolay Pultsin <geometer@fbreader.org>
2011-07-22 17:23:23 +0100

    progress indicator for android 1.5 devices

--- src/org/geometerplus/android/fbreader/library/LibraryActivity.java
+++ src/org/geometerplus/android/fbreader/library/LibraryActivity.java
@@ -76,6 +76,9 @@ public class LibraryActivity extends BaseActivity implements MenuItem.OnMenuItem
 		}
 
 		final ListAdapter adapter = new LibraryListAdapter(this);
+		if ("3".equals(new android.os.Build.VERSION().SDK)) {
+			getWindow().setFeatureInt(Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+		}
 		init(getIntent());
 
 		getListView().setTextFilterEnabled(true);
