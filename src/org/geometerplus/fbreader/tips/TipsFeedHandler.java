893acbf2b0387c6ab9387767ad485c5ba87229f8
Nikolay Pultsin <geometer@fbreader.org>
2011-10-23 16:40:19 +0100

    "abstract" part of tips subsystem (from 'tips' branch)

---
+++ src/org/geometerplus/fbreader/tips/TipsFeedHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2009-2011 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+package org.geometerplus.fbreader.tips;
+
+import java.util.*;
+
+import org.geometerplus.zlibrary.core.filesystem.ZLFile;
+
+import org.geometerplus.fbreader.network.atom.*;
+
+public class TipsFeedHandler extends AbstractATOMFeedHandler {
+	public static List<Tip> collectTips(ZLFile file) {
+		final TipsFeedHandler handler = new TipsFeedHandler();
+		new ATOMXMLReader(handler, false).read(file);
+		return Collections.unmodifiableList(handler.myTips);
+	}
+
+	private final List<Tip> myTips = new LinkedList<Tip>();
+
+	@Override
+	public boolean processFeedEntry(ATOMEntry entry) {
+		myTips.add(new Tip(entry.Title, entry.Content));
+		return false;
+	}
+}
