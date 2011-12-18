81d1e1a34f5ae40faa5cd54a928238bc032616d7
Nikolay Pultsin <geometer@fbreader.org>
2011-12-18 22:49:08 +0000

    fixed opensearch request

--- src/org/geometerplus/fbreader/network/opds/OPDSCustomNetworkLink.java
+++ src/org/geometerplus/fbreader/network/opds/OPDSCustomNetworkLink.java
@@ -131,7 +131,7 @@ public class OPDSCustomNetworkLink extends OPDSNetworkLink implements ICustomNet
 		if (!opensearchDescriptionURLs.isEmpty()) {
 			LinkedList<ZLNetworkRequest> requests = new LinkedList<ZLNetworkRequest>();
 			for (String url: opensearchDescriptionURLs) {
-				requests.add(new ZLNetworkRequest(url) {
+				requests.add(new ZLNetworkRequest(url, quietly) {
 					@Override
 					public void handleStream(InputStream inputStream, int length) throws IOException, ZLNetworkException {
 						new OpenSearchXMLReader(getURL(), descriptions).read(inputStream);
