bfec7598392e9f55631533ea7d29743eaa4609f2
Nikolay Pultsin <geometer@fbreader.org>
2011-12-22 02:44:08 +0000

    ScanLocalNetworkActivity introduced

--- src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
+++ src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
@@ -19,7 +19,118 @@
 
 package org.geometerplus.android.fbreader.network;
 
-import android.app.ListActivity;
+import java.util.ArrayList;
 
+import android.app.ListActivity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import javax.jmdns.*;
+
+import org.geometerplus.zlibrary.core.resources.ZLResource;
+
+import org.geometerplus.fbreader.network.NetworkLibrary;
+
+import org.geometerplus.zlibrary.ui.android.R;
+
+import org.geometerplus.android.util.UIUtil;
+ 
 public class ScanLocalNetworkActivity extends ListActivity {
+	private final static String[] ourServiceTypes = { "_stanza._tcp.local." };
+
+	private final ZLResource myResource =
+		NetworkLibrary.Instance().resource().getResource("addCatalog");
+
+	private WifiManager.MulticastLock myLock;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		Thread.setDefaultUncaughtExceptionHandler(new org.geometerplus.zlibrary.ui.android.library.UncaughtExceptionHandler(this));
+
+		setContentView(R.layout.scan_local_network);
+
+		setTitle(myResource.getResource("localCatalogs").getValue());
+
+		final View buttonView = findViewById(R.id.scan_local_network_buttons);
+		final ZLResource buttonResource = ZLResource.resource("dialog").getResource("button");
+
+		final Button rescanButton = (Button)buttonView.findViewById(R.id.ok_button);
+		rescanButton.setText(buttonResource.getResource("rescan").getValue());
+		rescanButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				scan();
+			}
+		});
+
+		final Button cancelButton = (Button)buttonView.findViewById(R.id.cancel_button);
+		cancelButton.setText(buttonResource.getResource("cancel").getValue());
+		cancelButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				finish();
+			}
+		});
+
+		final WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+		myLock = wifi.createMulticastLock("FBReader_lock");
+		myLock.setReferenceCounted(true);
+		myLock.acquire();
+
+		scan();
+	}
+
+	private void scan() {
+		final Runnable scanRunnable = new Runnable() {
+			public void run() {
+				final ArrayList<ServiceInfoItem> services = new ArrayList<ServiceInfoItem>();
+				String errorText;
+
+				try {
+					final JmDNS mcDNS = JmDNS.create();
+					for (String type : ourServiceTypes) {
+						for (ServiceInfo info : mcDNS.list(type)) {
+							services.add(new ServiceInfoItem(info));
+						}
+					}
+					errorText = services.isEmpty()
+						? myResource.getResource("noCatalogsFound").getValue() : null;
+				} catch (Exception e) {
+					errorText = e.getMessage();
+				}
+
+				setup(services, errorText);
+			}
+		};
+		UIUtil.wait("scanningLocalNetwork", scanRunnable, this);
+	}
+
+	private void setup(final ArrayList<ServiceInfoItem> services, final String errorText) {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				setListAdapter(new ArrayAdapter<ServiceInfoItem>(
+					ScanLocalNetworkActivity.this,
+					R.layout.local_service_item,
+					services
+				));
+				final View listView = findViewById(android.R.id.list);
+				final View errorView = findViewById(R.id.scan_local_network_error);
+				if (errorText != null) {
+					listView.setVisibility(View.GONE);
+					errorView.setVisibility(View.VISIBLE);
+					((TextView)errorView).setText(errorText);
+				} else {
+					listView.setVisibility(View.VISIBLE);
+					errorView.setVisibility(View.GONE);
+				}
+			}
+		});
+	}
+
+	private static class ServiceInfoItem {
+		ServiceInfoItem(ServiceInfo info) {
+		}
+	}
 }
