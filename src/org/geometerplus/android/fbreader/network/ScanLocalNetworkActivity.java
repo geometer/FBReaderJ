f161a31831181134b3183793ff1f44e780e79da9
Nikolay Pultsin <geometer@fbreader.org>
2011-12-24 19:28:45 +0000

    activity instead of dialog for WiFi turning on

--- src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
+++ src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
@@ -22,18 +22,18 @@ package org.geometerplus.android.fbreader.network;
 import java.util.*;
 import java.net.*;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface.OnClickListener;
-import android.content.DialogInterface;
 
 import javax.jmdns.*;
 
@@ -44,7 +44,7 @@ import org.geometerplus.fbreader.network.NetworkLibrary;
 import org.geometerplus.zlibrary.ui.android.R;
 
 import org.geometerplus.android.util.UIUtil;
- 
+
 public class ScanLocalNetworkActivity extends ListActivity {
 	private final static String[] ourServiceTypes = { "_stanza._tcp.local." };
 
@@ -52,7 +52,6 @@ public class ScanLocalNetworkActivity extends ListActivity {
 		NetworkLibrary.Instance().resource().getResource("addCatalog");
 
 	private WifiManager.MulticastLock myLock;
-	private WifiManager myWifi;
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -66,14 +65,6 @@ public class ScanLocalNetworkActivity extends ListActivity {
 		final View buttonView = findViewById(R.id.scan_local_network_buttons);
 		final ZLResource buttonResource = ZLResource.resource("dialog").getResource("button");
 
-		final Button rescanButton = (Button)buttonView.findViewById(R.id.ok_button);
-		rescanButton.setText(buttonResource.getResource("rescan").getValue());
-		rescanButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				scan();
-			}
-		});
-
 		final Button cancelButton = (Button)buttonView.findViewById(R.id.cancel_button);
 		cancelButton.setText(buttonResource.getResource("cancel").getValue());
 		cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -82,12 +73,41 @@ public class ScanLocalNetworkActivity extends ListActivity {
 			}
 		});
 
-		myWifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-		myLock = myWifi.createMulticastLock("FBReader_lock");
-		myLock.setReferenceCounted(true);
-		myLock.acquire();
+		final WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+		final int state = wifiManager.getWifiState();
+		if (state != WifiManager.WIFI_STATE_ENABLED && state != WifiManager.WIFI_STATE_ENABLING) {
+			setTitle(myResource.getResource("wifiIsTurnedOff").getValue());
+			final View listView = findViewById(android.R.id.list);
+			final TextView errorView = (TextView)findViewById(R.id.scan_local_network_error);
+			listView.setVisibility(View.GONE);
+			errorView.setVisibility(View.VISIBLE);
+			errorView.setText(myResource.getResource("turnWiFiOn").getValue());
 
-		scan();
+			final Button turnOnButton = (Button)buttonView.findViewById(R.id.ok_button);
+			turnOnButton.setText(buttonResource.getResource("turnOn").getValue());
+			turnOnButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					wifiManager.setWifiEnabled(true);
+					finish();
+				}
+			});
+
+			myLock = null;
+		} else {
+			final Button rescanButton = (Button)buttonView.findViewById(R.id.ok_button);
+			rescanButton.setText(buttonResource.getResource("rescan").getValue());
+			rescanButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					scan();
+				}
+			});
+
+			myLock = wifiManager.createMulticastLock("FBReader_lock");
+			myLock.setReferenceCounted(true);
+			myLock.acquire();
+
+			scan();
+		}
 	}
 
 	@Override
@@ -100,10 +120,19 @@ public class ScanLocalNetworkActivity extends ListActivity {
 
 	private List<InetAddress> getLocalIpAddresses() {
 		final List<InetAddress> addresses = new LinkedList<InetAddress>();
+		Method testPtoPMethod = null;
+		try {
+			testPtoPMethod = NetworkInterface.class.getMethod("isPointToPoint");
+		} catch (NoSuchMethodException e) {
+		}
 		try {
 			for (NetworkInterface iface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-				if (iface.isPointToPoint()) {
-					continue;
+				try {
+					if (testPtoPMethod != null && (Boolean)testPtoPMethod.invoke(iface)) {
+						continue;
+					}
+				} catch (IllegalAccessException e) {
+				} catch (InvocationTargetException e) {
 				}
 				for (InetAddress addr : Collections.list(iface.getInetAddresses())) {
 					if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
@@ -118,9 +147,6 @@ public class ScanLocalNetworkActivity extends ListActivity {
 	}
 
 	private void scan() {
-		if (myWifi.getWifiState() != WifiManager.WIFI_STATE_ENABLED && myWifi.getWifiState() != WifiManager.WIFI_STATE_ENABLING) {
-			showWifiDialog();
-		}
 		final Runnable scanRunnable = new Runnable() {
 			public void run() {
 				final ArrayList<ServiceInfoItem> services = new ArrayList<ServiceInfoItem>();
@@ -154,36 +180,8 @@ public class ScanLocalNetworkActivity extends ListActivity {
 		UIUtil.wait("scanningLocalNetwork", scanRunnable, this);
 	}
 
-	private void showWifiDialog() {
-		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-		alertDialog.setTitle(myResource.getResource("wifiDisabled").getValue());
-		alertDialog.setMessage(myResource.getResource("enableWifi").getValue());
-		final ZLResource buttonResource = ZLResource.resource("dialog").getResource("button");
-		alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, buttonResource.getResource("ok").getValue(), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				final Runnable wifiRunnable = new Runnable() {
-					public void run() {
-						if (!myWifi.setWifiEnabled(true)) {
-							finish();
-						}
-						while(!myWifi.isWifiEnabled()) {
-						}
-					}
-				};
-				UIUtil.wait("enablingWifi", wifiRunnable, ScanLocalNetworkActivity.this);
-				scan();
-			}
-		});
-		alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, buttonResource.getResource("cancel").getValue(), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				finish();
-			}
-		});
-		alertDialog.show();
-	}
-
-
 	private void setup(final ArrayList<ServiceInfoItem> services, final String errorText) {
+		System.err.println("setup " + services.size() + " " + errorText);
 		runOnUiThread(new Runnable() {
 			public void run() {
 				setListAdapter(new ArrayAdapter<ServiceInfoItem>(
@@ -192,11 +190,12 @@ public class ScanLocalNetworkActivity extends ListActivity {
 					services
 				));
 				final View listView = findViewById(android.R.id.list);
-				final View errorView = findViewById(R.id.scan_local_network_error);
+				final TextView errorView = (TextView)findViewById(R.id.scan_local_network_error);
 				if (errorText != null) {
 					listView.setVisibility(View.GONE);
 					errorView.setVisibility(View.VISIBLE);
-					((TextView)errorView).setText(errorText);
+					errorView.setText(errorText);
+					errorView.setTextColor(Color.RED);
 				} else {
 					listView.setVisibility(View.VISIBLE);
 					errorView.setVisibility(View.GONE);
