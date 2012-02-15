b72243aca76f1cab42c2cc62ea7d256f7cbff74a
Nikolay Pultsin <geometer@fbreader.org>
2012-02-15 03:07:12 +0000

    asynchronous scanning for Calibre servers

--- src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
+++ src/org/geometerplus/android/fbreader/network/ScanLocalNetworkActivity.java
@@ -21,15 +21,15 @@ package org.geometerplus.android.fbreader.network;
 
 import java.util.*;
 import java.net.*;
+import java.io.IOException;
 
 import android.app.ListActivity;
-import android.content.Context;
-import android.content.Intent;
+import android.content.*;
 import android.graphics.Color;
-import android.net.Uri;
+import android.net.*;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
 import android.widget.*;
 
 import javax.jmdns.*;
@@ -43,8 +43,6 @@ import org.geometerplus.zlibrary.ui.android.R;
 import org.geometerplus.android.util.UIUtil;
 
 public class ScanLocalNetworkActivity extends ListActivity {
-	private final static String[] ourServiceTypes = { "_stanza._tcp.local." };
-
 	private final ZLResource myResource =
 		NetworkLibrary.Instance().resource().getResource("addCatalog");
 
@@ -56,6 +54,8 @@ public class ScanLocalNetworkActivity extends ListActivity {
 		Thread.setDefaultUncaughtExceptionHandler(new org.geometerplus.zlibrary.ui.android.library.UncaughtExceptionHandler(this));
 
 		setContentView(R.layout.scan_local_network);
+
+		setListAdapter(new ItemAdapter());
 
 		setTitle(myResource.getResource("localCatalogs").getValue());
 
@@ -99,7 +99,12 @@ public class ScanLocalNetworkActivity extends ListActivity {
 			rescanButton.setText(buttonResource.getResource("rescan").getValue());
 			rescanButton.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View view) {
-					scan();
+					runOnUiThread(new Runnable() {
+						public void run() {
+							clear();
+							scan();
+						}
+					});
 				}
 			});
 
@@ -113,10 +118,10 @@ public class ScanLocalNetworkActivity extends ListActivity {
 
 	@Override
 	protected void onDestroy() {
-		super.onDestroy();
 		if (myLock != null) {
 			myLock.release();
 		}
+		super.onDestroy();
 	}
 
 	private List<InetAddress> getLocalIpAddresses() {
@@ -138,92 +143,295 @@ public class ScanLocalNetworkActivity extends ListActivity {
 		return addresses;
 	}
 
-	private void scan() {
-		final Runnable scanRunnable = new Runnable() {
-			public void run() {
-				final ArrayList<ServiceInfoItem> services = new ArrayList<ServiceInfoItem>();
-				String errorText = null;
+	private class ServiceCollector implements ServiceListener {
+		private final static String STANZA_ZEROCONF_TYPE = "_stanza._tcp.local.";
+		private final static String CALIBRE_ZEROCONF_TYPE = "_calibre._tcp.local.";
+		private final static String OPDS_ZEROCONF_TYPE = "_opds._tcp.local.";
 
-				try {
-					final List<InetAddress> addresses = getLocalIpAddresses();
-					if (addresses.isEmpty()) {
-						errorText = myResource.getResource("noLocalConnection").getValue();
-					} else {
-						for (InetAddress address : addresses) {
-							final JmDNS mcDNS = JmDNS.create(address, "FBReader");
-							for (String type : ourServiceTypes) {
-								for (ServiceInfo info : mcDNS.list(type)) {
-									services.add(new ServiceInfoItem(info));
-								}
+		private JmDNS myMCDNS;
+
+		ServiceCollector(InetAddress address) {
+			try {
+				myMCDNS = JmDNS.create(address, "FBReader");
+			} catch (IOException e) {
+				return;
+			}
+			myMCDNS.addServiceListener(STANZA_ZEROCONF_TYPE, this);
+			myMCDNS.addServiceListener(CALIBRE_ZEROCONF_TYPE, this);
+			myMCDNS.addServiceListener(OPDS_ZEROCONF_TYPE, this);
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					getListAdapter().addWaitItem();
+				}
+			});
+			final Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						public void run() {
+							final ItemAdapter adapter = getListAdapter();
+							if (adapter.removeWaitItem() && adapter.getCount() == 0) {
+								setErrorText(myResource.getResource("noCatalogsFound").getValue());
 							}
-							mcDNS.close();
 						}
-						if (services.isEmpty()) {
-							errorText = myResource.getResource("noCatalogsFound").getValue();
-						}
+					});
+					System.err.println("Cancelling JmDNS");
+					try {
+						myMCDNS.close();
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-				} catch (Exception e) {
-					errorText = e.getMessage();
+					timer.cancel();
 				}
-
-				setup(services, errorText);
-			}
-		};
-		UIUtil.wait("scanningLocalNetwork", scanRunnable, this);
-	}
-
-	private void setup(final ArrayList<ServiceInfoItem> services, final String errorText) {
-		runOnUiThread(new Runnable() {
-			public void run() {
-				setListAdapter(new ArrayAdapter<ServiceInfoItem>(
-					ScanLocalNetworkActivity.this,
-					R.layout.local_service_item,
-					services
-				));
-				final View listView = findViewById(android.R.id.list);
-				final TextView errorView = (TextView)findViewById(R.id.scan_local_network_error);
-				if (errorText != null) {
-					listView.setVisibility(View.GONE);
-					errorView.setVisibility(View.VISIBLE);
-					errorView.setText(errorText);
-					errorView.setTextColor(Color.RED);
-				} else {
-					listView.setVisibility(View.VISIBLE);
-					errorView.setVisibility(View.GONE);
-				}
-			}
-		});
-	}
-
-	private static class ServiceInfoItem {
-		private final ServiceInfo myServiceInfo;
-
-		public ServiceInfoItem(ServiceInfo info) {
-			myServiceInfo = info;
+			}, 10000);
 		}
 
-		public String toString() {
-			return myServiceInfo.getName();
+		public void serviceAdded(ServiceEvent event) {
+			ServiceInfo info = event.getInfo();
+			if (info == null || !info.hasData()) {
+				info = myMCDNS.getServiceInfo(event.getType(), event.getName(), true);
+			}
+			addInfo(info);
 		}
 
-		public String getUrl() {
-			return myServiceInfo.getURLs()[0];
+		public void serviceRemoved(ServiceEvent event) {
+			System.err.println("removing: " + event);
+		}
+
+		public void serviceResolved(ServiceEvent event) {
+			addInfo(event.getInfo());
+		}
+
+		private void addInfo(final ServiceInfo info) {
+			if (info == null || !info.hasData()) {
+				return;
+			}
+
+			final String path = info.getPropertyString("path");
+			if (path == null) {
+				return;
+			}
+
+			final String[] urls = info.getURLs();
+			if (urls.length != 1) {
+				return;
+			}
+
+			if (urls[0] == null || !urls[0].endsWith(path)) {
+				return;
+			}
+
+			final String type = info.getType();
+			if (STANZA_ZEROCONF_TYPE.equals(info.getType()) || "/stanza".equals(path)) {
+				urls[0] = urls[0].substring(0, urls[0].length() - path.length()) + "/opds";
+			}
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					getListAdapter().addServiceItem(info.getName(), urls[0]);
+				}
+			});
+		}
+	}
+
+	private void scan() {
+		final List<InetAddress> addresses = getLocalIpAddresses();
+		if (addresses.isEmpty()) {
+			runOnUiThread(new Runnable() {
+				public void run() {
+					setErrorText(myResource.getResource("noLocalConnection").getValue());
+				}
+			});
+		} else {
+			for (final InetAddress a : addresses) {
+				new Thread() {
+					public void run() {
+						new ServiceCollector(a);
+					}
+				}.start();
+			}
+		}
+	}
+
+	private void clear() {
+		getListAdapter().clear();
+		final View listView = findViewById(android.R.id.list);
+		final TextView errorView = (TextView)findViewById(R.id.scan_local_network_error);
+		listView.setVisibility(View.VISIBLE);
+		errorView.setVisibility(View.GONE);
+	}
+
+	private void setErrorText(final String errorText) {
+		final View listView = findViewById(android.R.id.list);
+		final TextView errorView = (TextView)findViewById(R.id.scan_local_network_error);
+		listView.setVisibility(View.GONE);
+		errorView.setVisibility(View.VISIBLE);
+		errorView.setText(errorText);
+	}
+
+	private class ItemAdapter extends BaseAdapter {
+		final private WaitItem myWaitItem = new WaitItem(
+			myResource.getResource("scanningLocalNetwork").getValue()
+		);
+		private volatile int myWaitItemCount;
+		private final ArrayList<ServiceInfoItem> myItems = new ArrayList<ServiceInfoItem>();
+
+		@Override
+		public Item getItem(int position) {
+			try {
+				return myItems.get(position);
+			} catch (Exception e) {
+				return myWaitItem;
+			}
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public synchronized int getCount() {
+			return myWaitItemCount > 0 ? myItems.size() + 1 : myItems.size();
+		}
+
+		synchronized void clear() {
+			myItems.clear();
+			myWaitItemCount = 0;
+			notifyDataSetChanged();
+		}
+
+		synchronized boolean addWaitItem() {
+			if (myWaitItemCount++ == 0) {
+				notifyDataSetChanged();
+				findViewById(R.id.scan_local_network_container).invalidate();
+				return true;
+			}
+			return false;
+		}
+
+		synchronized boolean removeWaitItem() {
+			if (myWaitItemCount == 0) {
+				return false;
+			}
+			if (--myWaitItemCount == 0) {
+				notifyDataSetChanged();
+				findViewById(R.id.scan_local_network_container).invalidate();
+				return true;
+			}
+			return false;
+		}
+
+		synchronized void addServiceItem(String name, String url) {
+			try {
+				final ServiceInfoItem item = new ServiceInfoItem(name, Uri.parse(url));
+				if (!myItems.contains(item)) {
+					myItems.add(item);
+					notifyDataSetChanged();
+					findViewById(R.id.scan_local_network_container).invalidate();
+				}
+			} catch (ParseException e) {
+			}
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final Item item = getItem(position);
+			final View view;
+			if (convertView == null) {
+				view = LayoutInflater.from(ScanLocalNetworkActivity.this).inflate(R.layout.local_service_item, parent, false);
+			} else {
+				view = convertView;
+			}
+
+			final TextView textView = (TextView)view.findViewById(R.id.local_service_text);
+			final ImageView iconView = (ImageView)view.findViewById(R.id.local_service_icon);
+			final ProgressBar progress = (ProgressBar)view.findViewById(R.id.local_service_progress);
+			if (convertView == null) {
+
+				view.measure(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+				final int h = view.getMeasuredHeight() * 6 / 10;
+				iconView.getLayoutParams().width = h;
+				iconView.getLayoutParams().height = h;
+				iconView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+				iconView.requestLayout();
+				progress.getLayoutParams().width = h;
+				progress.getLayoutParams().height = h;
+				iconView.requestLayout();
+			}
+
+			textView.setText(item.Name);
+
+			if (item instanceof ServiceInfoItem) {
+				iconView.setVisibility(View.VISIBLE);
+				progress.setVisibility(View.GONE);
+				iconView.setImageResource(R.drawable.ic_list_library_calibre);
+			} else /* item instanceof WaitItem */ {
+				iconView.setVisibility(View.GONE);
+				progress.setVisibility(View.VISIBLE);
+			}
+
+			return view;
+		}
+	}
+
+	private static abstract class Item {
+		public final String Name;
+
+		public Item(String name) {
+			Name = name;
+		}
+	}
+
+	private static class ServiceInfoItem extends Item {
+		public final Uri URI;
+
+		public ServiceInfoItem(String name, Uri uri) {
+			super(name);
+			URI = uri;
+		}
+
+		@Override
+		public int hashCode() {
+			return Name.hashCode() + URI.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			return
+				o instanceof ServiceInfoItem &&
+				Name.equals(((ServiceInfoItem)o).Name) &&
+				URI.equals(((ServiceInfoItem)o).URI);
+		}
+	}
+
+	private static class WaitItem extends Item {
+		public WaitItem(String name) {
+			super(name);
 		}
 	}
 
 	@Override
 	protected void onListItemClick(ListView parent, View view, int position, long id) {
-		final ServiceInfoItem item = (ServiceInfoItem)getListAdapter().getItem(position);
-		try {
-			startActivity(new Intent(
-				Intent.ACTION_VIEW,
-				Uri.parse(item.getUrl()),
-				getApplicationContext(),
-				AddCustomCatalogActivity.class
-			));
-			finish();
-		} catch (Exception e) {
-			// TODO: show an error message
+		final Item item = getListAdapter().getItem(position);
+		if (item instanceof ServiceInfoItem) {
+			try {
+				startActivity(new Intent(
+					Intent.ACTION_VIEW,
+					((ServiceInfoItem)item).URI,
+					getApplicationContext(),
+					AddCustomCatalogActivity.class
+				));
+				finish();
+			} catch (ActivityNotFoundException e) {
+			}
 		}
+	}
+
+	@Override
+	public ItemAdapter getListAdapter() {
+		return (ItemAdapter)super.getListAdapter();
 	}
 }
