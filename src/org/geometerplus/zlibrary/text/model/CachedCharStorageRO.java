8ef5301a138f75c9d7b93dc5c30057ed1ff108bb
Nikolay Pultsin <geometer@fbreader.org>
2012-02-22 04:56:39 +0000

    read-only version of CachedCharStorage

---
+++ src/org/geometerplus/zlibrary/text/model/CachedCharStorageRO.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2007-2012 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+package org.geometerplus.zlibrary.text.model;
+
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+
+public final class CachedCharStorageRO extends CachedCharStorageBase {
+	public CachedCharStorageRO(String directoryName, String fileExtension, int blocksNumber) {
+		super(directoryName, fileExtension);
+		myArray.addAll(Collections.nCopies(blocksNumber, new WeakReference<char[]>(null)));
+	}
+
+	public char[] createNewBlock(int minimumLength) {
+		throw new UnsupportedOperationException("CachedCharStorageRO is a read-only storage.");
+	}
+
+	public void freezeLastBlock() {
+	}
+}
