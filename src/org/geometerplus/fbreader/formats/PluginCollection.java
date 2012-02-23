f39ad4d282b1857a84e6a8de9474974bcb5065ed
Nikolay Pultsin <geometer@fbreader.org>
2012-02-23 16:05:39 +0100

    synchronization with native branch

--- src/org/geometerplus/fbreader/formats/PluginCollection.java
+++ src/org/geometerplus/fbreader/formats/PluginCollection.java
@@ -27,11 +27,13 @@ import org.geometerplus.zlibrary.core.filesystem.ZLFile;
 import org.geometerplus.fbreader.formats.fb2.FB2Plugin;
 import org.geometerplus.fbreader.formats.oeb.OEBPlugin;
 import org.geometerplus.fbreader.formats.pdb.MobipocketPlugin;
+import org.geometerplus.fbreader.filetype.*;
 
 public class PluginCollection {
 	private static PluginCollection ourInstance;
 
-	private final ArrayList<FormatPlugin> myPlugins = new ArrayList<FormatPlugin>();
+	private final Map<FormatPlugin.Type,List<FormatPlugin>> myPlugins =
+		new HashMap<FormatPlugin.Type,List<FormatPlugin>>();
 	public ZLStringOption DefaultLanguageOption;
 	public ZLStringOption DefaultEncodingOption;
 	public ZLBooleanOption LanguageAutoDetectOption;
@@ -39,9 +41,6 @@ public class PluginCollection {
 	public static PluginCollection Instance() {
 		if (ourInstance == null) {
 			ourInstance = new PluginCollection();
-			ourInstance.myPlugins.add(new FB2Plugin());
-			ourInstance.myPlugins.add(new MobipocketPlugin());
-			ourInstance.myPlugins.add(new OEBPlugin());
 		}
 		return ourInstance;
 	}
@@ -56,14 +55,53 @@ public class PluginCollection {
 		LanguageAutoDetectOption = new ZLBooleanOption("Format", "AutoDetect", true);
 		DefaultLanguageOption = new ZLStringOption("Format", "DefaultLanguage", "en");
 		DefaultEncodingOption = new ZLStringOption("Format", "DefaultEncoding", "windows-1252");
+
+		addPlugin(new FB2Plugin());
+		addPlugin(new MobipocketPlugin());
+		addPlugin(new OEBPlugin());
+	}
+
+	private void addPlugin(FormatPlugin plugin) {
+		final FormatPlugin.Type type = plugin.type();
+		List<FormatPlugin> list = myPlugins.get(type);
+		if (list == null) {
+			list = new ArrayList<FormatPlugin>();
+			myPlugins.put(type, list);
+		}
+		list.add(plugin);
 	}
 
 	public FormatPlugin getPlugin(ZLFile file) {
-		for (FormatPlugin plugin : myPlugins) {
-			if (plugin.acceptsFile(file)) {
-				return plugin;
-			}
-		}
-		return null;
+		return getPlugin(file, FormatPlugin.Type.ANY);
 	}
+
+	public FormatPlugin getPlugin(ZLFile file, FormatPlugin.Type formatType) {
+		final FileType fileType = FileTypeCollection.Instance.typeForFile(file);
+		return getPlugin(fileType, formatType);
+	}
+
+	public FormatPlugin getPlugin(FileType fileType, FormatPlugin.Type formatType) {
+		if (fileType == null) {
+			return null;
+		}
+
+		if (formatType == FormatPlugin.Type.ANY) {
+			FormatPlugin p = getPlugin(fileType, FormatPlugin.Type.JAVA);
+			if (p == null) {
+				p = getPlugin(fileType, FormatPlugin.Type.NATIVE);
+			}
+			return p;
+		} else {
+			final List<FormatPlugin> list = myPlugins.get(formatType);
+			if (list == null) {
+				return null;
+			}
+			for (FormatPlugin p : list) {
+				if (fileType.Id.equalsIgnoreCase(p.supportedFileType())) {
+					return p;
+				}
+			}
+			return null;
+ 		}
+ 	}
 }
