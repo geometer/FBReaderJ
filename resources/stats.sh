13190d2788d68aa7df15217a515ea6230e5845f9
Nikolay Pultsin <geometer@fbreader.org>
2012-04-12 14:53:58 +0200

    resources checking scripts

---
+++ resources/stats.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for file in ../assets/resources/application/*.xml; do
+  lfile=`echo $file | sed "s/application/zlibrary/"`
+  lang=`basename $file .xml;`
+  all_count=`fgrep 'value="' $file | wc | awk '{ print $1 }'`
+  all_lcount=`fgrep 'value="' $lfile | wc | awk '{ print $1 }'`
+  neg_count=`fgrep toBeTranslated $file | wc | awk '{ print $1 }'`
+  neg_lcount=`fgrep toBeTranslated $lfile | wc | awk '{ print $1 }'`
+	echo $lang $(($neg_count+$neg_lcount)) of $(($all_count+$all_lcount))
+done
