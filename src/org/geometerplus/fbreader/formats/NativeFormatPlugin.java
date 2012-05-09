b75596d991c3df16581cc46dbc5feb7c9e3904c3
Nikolay Pultsin <geometer@fbreader.org>
2012-05-09 01:16:11 +0100

    separate plugin for native ePub support

--- src/org/geometerplus/fbreader/formats/NativeFormatPlugin.java
+++ src/org/geometerplus/fbreader/formats/NativeFormatPlugin.java
@@ -29,11 +29,14 @@ import org.geometerplus.fbreader.bookmodel.BookModel;
 import org.geometerplus.fbreader.bookmodel.BookReadingException;
 import org.geometerplus.fbreader.library.Book;
 import org.geometerplus.fbreader.formats.fb2.FB2NativePlugin;
+import org.geometerplus.fbreader.formats.oeb.OEBNativePlugin;
 
 public class NativeFormatPlugin extends FormatPlugin {
 	public static NativeFormatPlugin create(String fileType) {
 		if ("fb2".equals(fileType)) {
 			return new FB2NativePlugin();
+		} else if ("ePub".equals(fileType)) {
+			return new OEBNativePlugin();
 		} else {
 			return new NativeFormatPlugin(fileType);
 		}
@@ -88,7 +91,7 @@ public class NativeFormatPlugin extends FormatPlugin {
 
 	protected native void readCoverInternal(ZLFile file, ZLImage[] box);
 
-	// FIXME: temporary implementation; implement as a native code
+	// FIXME: temporary implementation; implement as a native code (?)
 	@Override
 	public String readAnnotation(ZLFile file) {
 		final FormatPlugin plugin = PluginCollection.Instance().getPlugin(file, FormatPlugin.Type.JAVA);
@@ -105,7 +108,6 @@ public class NativeFormatPlugin extends FormatPlugin {
 
 	@Override
 	public EncodingCollection supportedEncodings() {
-		// TODO: implement
 		return JavaEncodingCollection.Instance();
 	}
 }
