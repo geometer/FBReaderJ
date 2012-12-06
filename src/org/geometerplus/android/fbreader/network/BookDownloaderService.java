93d5e9953d5b84418a4f37ebbe24429c4a271655
dlmv <danya.malcev@gmail.com>
2012-12-06 15:47:58 +0400

    kill plugin if need

--- src/org/geometerplus/android/fbreader/network/BookDownloaderService.java
+++ src/org/geometerplus/android/fbreader/network/BookDownloaderService.java
@@ -197,6 +197,7 @@ public class BookDownloaderService extends Service {
 		if (file != null) {
 			intent.setAction(Intent.ACTION_VIEW).setData(Uri.fromFile(file));
 		}
+		intent.putExtra("KILL_PLUGIN", true);
 		return intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 	}
 
