b21459cdd04dcea85afdd61e0ca24c33dc036159
Nikolay Pultsin <geometer@fbreader.org>
2013-01-13 07:47:55 +0400

    jni/proguard changes for new package structure

--- jni/NativeFormats/util/AndroidUtil.cpp
+++ jni/NativeFormats/util/AndroidUtil.cpp
@@ -47,8 +47,8 @@ JavaClass AndroidUtil::Class_JavaEncodingCollection("org/geometerplus/zlibrary/c
 JavaClass AndroidUtil::Class_NativeFormatPlugin("org/geometerplus/fbreader/formats/NativeFormatPlugin");
 JavaClass AndroidUtil::Class_PluginCollection("org/geometerplus/fbreader/formats/PluginCollection");
 JavaClass AndroidUtil::Class_Paths("org/geometerplus/fbreader/Paths");
-JavaClass AndroidUtil::Class_Book("org/geometerplus/fbreader/library/Book");
-JavaClass AndroidUtil::Class_Tag("org/geometerplus/fbreader/library/Tag");
+JavaClass AndroidUtil::Class_Book("org/geometerplus/fbreader/book/Book");
+JavaClass AndroidUtil::Class_Tag("org/geometerplus/fbreader/book/Tag");
 JavaClass AndroidUtil::Class_NativeBookModel("org/geometerplus/fbreader/bookmodel/NativeBookModel");
 
 shared_ptr<StringMethod> AndroidUtil::Method_java_lang_String_toLowerCase;
@@ -178,9 +178,9 @@ bool AndroidUtil::init(JavaVM* jvm) {
 	Method_Book_setLanguage = new VoidMethod(Class_Book, "setLanguage", "(Ljava/lang/String;)");
 	Method_Book_setEncoding = new VoidMethod(Class_Book, "setEncoding", "(Ljava/lang/String;)");
 	Method_Book_addAuthor = new VoidMethod(Class_Book, "addAuthor", "(Ljava/lang/String;Ljava/lang/String;)");
-	Method_Book_addTag = new VoidMethod(Class_Book, "addTag", "(Lorg/geometerplus/fbreader/library/Tag;)");
+	Method_Book_addTag = new VoidMethod(Class_Book, "addTag", "(Lorg/geometerplus/fbreader/book/Tag;)");
 
-	StaticMethod_Tag_getTag = new StaticObjectMethod(Class_Tag, "getTag", Class_Tag, "(Lorg/geometerplus/fbreader/library/Tag;Ljava/lang/String;)");
+	StaticMethod_Tag_getTag = new StaticObjectMethod(Class_Tag, "getTag", Class_Tag, "(Lorg/geometerplus/fbreader/book/Tag;Ljava/lang/String;)");
 
 	Field_NativeBookModel_Book = new ObjectField(Class_NativeBookModel, "Book", Class_Book);
 	Method_NativeBookModel_initInternalHyperlinks = new VoidMethod(Class_NativeBookModel, "initInternalHyperlinks", "(Ljava/lang/String;Ljava/lang/String;I)");
