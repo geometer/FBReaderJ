796d4298435b53b49ba41aa59a2a7f22ff9434ed
dlmv <danya.malcev@gmail.com>
2013-01-17 16:56:46 +0400

    dialog fix + stringlist view

--- src/org/geometerplus/zlibrary/ui/android/library/ZLAndroidActivity.java
+++ src/org/geometerplus/zlibrary/ui/android/library/ZLAndroidActivity.java
@@ -78,10 +78,10 @@ public abstract class ZLAndroidActivity extends Activity {
 			});
 		}
 
-		public void openFile(ZLFile f, String appData) {
+		public boolean openFile(ZLFile f, String appData) {
 			if (f == null) {
 				showErrorDialog("unzipFailed");
-				return;
+				return false;
 			}
 			String extension = f.getExtension();
 			Uri uri = Uri.parse("file://" + f.getPath());
@@ -93,12 +93,12 @@ public abstract class ZLAndroidActivity extends Activity {
 				LaunchIntent.setDataAndType(uri, type.Name);
 				try {
 					myActivity.startActivity(LaunchIntent);
-					return;
+					return true;
 				} catch (ActivityNotFoundException e) {
 				}
 			}
 			showErrorDialog("externalNotFound");
-			return;
+			return false;
 		}
 	}
 
@@ -154,11 +154,7 @@ public abstract class ZLAndroidActivity extends Activity {
 						if (((ZLAndroidActivity)myActivity).myIsPaused) {
 							((ZLAndroidActivity)myActivity).myDialogToShow = dialog;
 						} else {
-							myActivity.runOnUiThread(new Runnable() {
-								public void run() {
-									dialog.show();
-								}
-							});
+								dialog.show();
 						}
 					}
 			});
