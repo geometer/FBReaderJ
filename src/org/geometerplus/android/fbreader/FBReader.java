a7967d88ba9b4f1dbf58a5138b625fffdceb6380
Nikolay Pultsin <geometer@fbreader.org>
2013-02-08 19:45:36 +0000

    synchronization with library-service branch

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -49,6 +49,7 @@ import org.geometerplus.fbreader.tips.TipsManager;
 
 import org.geometerplus.android.fbreader.api.*;
 import org.geometerplus.android.fbreader.library.KillerCallback;
+import org.geometerplus.android.fbreader.libraryService.BookCollectionShadow;
 import org.geometerplus.android.fbreader.libraryService.SQLiteBooksDatabase;
 import org.geometerplus.android.fbreader.tips.TipsActivity;
 
@@ -178,7 +179,7 @@ public final class FBReader extends Activity {
 			if (SQLiteBooksDatabase.Instance() == null) {
 				new SQLiteBooksDatabase(this, "READER");
 			}
-			myFBReaderApp = new FBReaderApp();
+			myFBReaderApp = new FBReaderApp(new BookCollectionShadow());
 		}
 		myBook = null;
 
