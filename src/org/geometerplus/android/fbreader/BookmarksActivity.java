31247e39bf85e54e850c0583f39fc25c33750b89
Nikolay Pultsin <geometer@fbreader.org>
2013-03-05 03:23:05 +0400

    code simplification

--- src/org/geometerplus/android/fbreader/BookmarksActivity.java
+++ src/org/geometerplus/android/fbreader/BookmarksActivity.java
@@ -44,7 +44,7 @@ public class BookmarksActivity extends TabActivity implements MenuItem.OnMenuIte
 	private static final int DELETE_ITEM_ID = 2;
 
 	private final BookCollectionShadow myCollection = new BookCollectionShadow();
-	private volatile Book myBook;
+	private volatile long myBookId = -1;
 
 	private final Comparator<Bookmark> myComparator = new Bookmark.ByTimeComparator();
 
@@ -78,7 +78,9 @@ public class BookmarksActivity extends TabActivity implements MenuItem.OnMenuIte
 		final TabHost host = getTabHost();
 		LayoutInflater.from(this).inflate(R.layout.bookmarks, host.getTabContentView(), true);
 
-		myBook = SerializerUtil.deserializeBook(getIntent().getStringExtra(FBReader.BOOK_KEY));
+		final Book book =
+			SerializerUtil.deserializeBook(getIntent().getStringExtra(FBReader.BOOK_KEY));
+		myBookId = book != null ? book.getId() : -1;
 	}
 
 	private class Initializer implements Runnable {
@@ -93,11 +95,10 @@ public class BookmarksActivity extends TabActivity implements MenuItem.OnMenuIte
 				}
 
 				myAllBooksAdapter.addAll(allBookmarks);
-				if (myBook != null) {
+				if (myBookId != -1) {
 					final List<Bookmark> thisBookBookmarks = new LinkedList<Bookmark>();
-					final long bookId = myBook.getId();
 					for (Bookmark b : allBookmarks) {
-						if (b.getBookId() == bookId) {
+						if (b.getBookId() == myBookId) {
 							thisBookBookmarks.add(b);
 						}
 					}
@@ -117,7 +118,7 @@ public class BookmarksActivity extends TabActivity implements MenuItem.OnMenuIte
 					return;
 				}
 
-				if (myBook != null) {
+				if (myBookId != -1) {
 					myThisBookAdapter = new BookmarksAdapter(
 						createTab("thisBook", R.id.this_book), true
 					);
@@ -194,9 +195,7 @@ public class BookmarksActivity extends TabActivity implements MenuItem.OnMenuIte
 		} else {
 			mySearchResultsAdapter.clear();
 		}
-		for (Bookmark b : results) {
-			mySearchResultsAdapter.add(b);
-		}
+		mySearchResultsAdapter.addAll(results);
 		getTabHost().setCurrentTabByTag("found");
 	}
 
