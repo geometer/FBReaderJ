6538daef87a09f032a6f3d111fb12d648e29edd7
Dmitry Yuranov <d.yuranov@gmail.com>
2013-03-18 13:01:11 +0400

    groups and orders the imports; adds the copyright notices

--- src/org/geometerplus/fbreader/network/rss/RSSNetworkLink.java
+++ src/org/geometerplus/fbreader/network/rss/RSSNetworkLink.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010-2013 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 package org.geometerplus.fbreader.network.rss;
 
 import java.io.IOException;
@@ -5,19 +24,14 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.geometerplus.fbreader.network.AbstractNetworkLink;
-import org.geometerplus.fbreader.network.ICustomNetworkLink;
-import org.geometerplus.fbreader.network.NetworkCatalogItem;
-import org.geometerplus.fbreader.network.NetworkLibrary;
-import org.geometerplus.fbreader.network.NetworkOperationData;
-import org.geometerplus.fbreader.network.authentication.NetworkAuthenticationManager;
-import org.geometerplus.fbreader.network.tree.NetworkItemsLoader;
-import org.geometerplus.fbreader.network.urlInfo.UrlInfo;
-import org.geometerplus.fbreader.network.urlInfo.UrlInfoCollection;
-import org.geometerplus.fbreader.network.urlInfo.UrlInfoWithDate;
 import org.geometerplus.zlibrary.core.network.ZLNetworkException;
 import org.geometerplus.zlibrary.core.network.ZLNetworkRequest;
 import org.geometerplus.zlibrary.core.util.MimeType;
+
+import org.geometerplus.fbreader.network.*;
+import org.geometerplus.fbreader.network.urlInfo.*;
+import org.geometerplus.fbreader.network.authentication.NetworkAuthenticationManager;
+import org.geometerplus.fbreader.network.tree.NetworkItemsLoader;
 
 public class RSSNetworkLink extends AbstractNetworkLink implements ICustomNetworkLink {
 	
