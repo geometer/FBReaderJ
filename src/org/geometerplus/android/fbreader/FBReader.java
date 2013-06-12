b669828f2e3746305ce1736ccc3708dff6b11a90
Nikolay Pultsin <geometer@fbreader.org>
2013-06-12 19:43:32 +0400

    compilation issue

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -19,6 +19,7 @@
 
 package org.geometerplus.android.fbreader;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 import android.app.Activity;
@@ -33,6 +34,7 @@ import android.widget.RelativeLayout;
 
 import org.geometerplus.zlibrary.core.filesystem.ZLFile;
 import org.geometerplus.zlibrary.core.library.ZLibrary;
+import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 import org.geometerplus.zlibrary.text.view.ZLTextView;
 
