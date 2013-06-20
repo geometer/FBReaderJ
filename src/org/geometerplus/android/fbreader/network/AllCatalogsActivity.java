038a8a32275875e93ff9ebef226695f2a57e89d2
Dmitry Yuranov <d.yuranov@gmail.com>
2013-06-20 20:24:13 +0400

    Copyrights have been added

--- src/org/geometerplus/android/fbreader/network/AllCatalogsActivity.java
+++ src/org/geometerplus/android/fbreader/network/AllCatalogsActivity.java
@@ -1,29 +1,39 @@
+/*
+ * Copyright (C) 2010-2013 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 package org.geometerplus.android.fbreader.network;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.TreeSet;
-
-import org.geometerplus.android.fbreader.covers.CoverManager;
-import org.geometerplus.fbreader.network.INetworkLink;
-import org.geometerplus.fbreader.network.NetworkLibrary;
-import org.geometerplus.fbreader.network.NetworkTree;
-import org.geometerplus.fbreader.network.tree.NetworkCatalogRootTree;
-import org.geometerplus.zlibrary.ui.android.R;
+import java.util.*;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.*;
+import android.widget.*;
+
+import org.geometerplus.zlibrary.ui.android.R;
+import org.geometerplus.fbreader.network.*;
+import org.geometerplus.fbreader.network.tree.NetworkCatalogRootTree;
+import org.geometerplus.android.fbreader.covers.CoverManager;
+
+
 
 public class AllCatalogsActivity extends Activity {
 	final NetworkLibrary library = NetworkLibrary.Instance();
