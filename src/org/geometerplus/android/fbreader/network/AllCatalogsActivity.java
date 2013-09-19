5429088bc2d95ab8883a585f537a80a4015b93b4
Dmitry Yuranov <d.yuranov@gmail.com>
2013-09-19 20:05:48 +0400

    Fix result setting

--- src/org/geometerplus/android/fbreader/network/AllCatalogsActivity.java
+++ src/org/geometerplus/android/fbreader/network/AllCatalogsActivity.java
@@ -134,11 +134,11 @@ public class AllCatalogsActivity extends ListActivity {
 		if(item != null && item instanceof CatalogItem){
 			CatalogItem catalogItem = (CatalogItem)item;
 			if(catalogItem.IsChecked){
-				int insertIndex = index <=0 ?0:(index-1);
+				int insertIndex = index <= 0 ? -1 : (index-1);
 				if(mySelectedItems.contains(catalogItem)){
 					mySelectedItems.remove(catalogItem);
 				}
-				if(insertIndex > 0){
+				if(insertIndex >= 0){
 					mySelectedItems.add(insertIndex, catalogItem);
 				}else{
 					mySelectedItems.add(catalogItem);
