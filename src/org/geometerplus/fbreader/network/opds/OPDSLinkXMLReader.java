3623b5a65e83980dcee85cdfe1e0b54e6b6ca205
Nikolay Pultsin <geometer@fbreader.org>
2013-10-03 07:48:08 +0100

    formatting

--- src/org/geometerplus/fbreader/network/opds/OPDSLinkXMLReader.java
+++ src/org/geometerplus/fbreader/network/opds/OPDSLinkXMLReader.java
@@ -39,6 +39,7 @@ class OPDSLinkXMLReader extends OPDSXMLReader implements OPDSConstants {
 		private final LinkedList<URLRewritingRule> myUrlRewritingRules = new LinkedList<URLRewritingRule>();
 		private final HashMap<RelationAlias, String> myRelationAliases = new HashMap<RelationAlias, String>();
 		private final LinkedHashMap<String,String> myExtraData = new LinkedHashMap<String,String>();
+
 		List<INetworkLink> links() {
 			return myLinks;
 		}
