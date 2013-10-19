ddbda07d4d7520a8857c47aa6f86186bf7e184cf
Nikolay Pultsin <geometer@fbreader.org>
2013-10-19 14:27:09 +0100

    fixed behaviour

--- src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
+++ src/org/geometerplus/zlibrary/ui/android/view/ZLAndroidWidget.java
@@ -310,7 +310,7 @@ public class ZLAndroidWidget extends View implements ZLViewWidget, View.OnLongCl
 			case MotionEvent.ACTION_UP:
 				if (myPendingDoubleTap) {
 					view.onFingerDoubleTap(x, y);
-				} if (myLongClickPerformed) {
+				} else if (myLongClickPerformed) {
 					view.onFingerReleaseAfterLongPress(x, y);
 				} else {
 					if (myPendingLongClickRunnable != null) {
