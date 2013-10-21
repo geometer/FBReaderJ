ab5783ec4468ec17bef31559621c8877036006e4
Nikolay Pultsin <geometer@fbreader.org>
2013-10-21 01:13:40 +0100

    imports order
    Conflicts:
    
    	src/org/geometerplus/android/fbreader/library/BookInfoActivity.java

--- src/org/geometerplus/android/fbreader/library/BookInfoActivity.java
+++ src/org/geometerplus/android/fbreader/library/BookInfoActivity.java
@@ -37,9 +37,9 @@ import android.widget.*;
 import org.geometerplus.zlibrary.core.filesystem.ZLPhysicalFile;
 import org.geometerplus.zlibrary.core.image.ZLImage;
 import org.geometerplus.zlibrary.core.image.ZLLoadableImage;
-import org.geometerplus.zlibrary.core.resources.ZLResource;
 import org.geometerplus.zlibrary.core.language.Language;
 import org.geometerplus.zlibrary.core.language.ZLLanguageUtil;
+import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 import org.geometerplus.zlibrary.ui.android.R;
 import org.geometerplus.zlibrary.ui.android.image.ZLAndroidImageData;
@@ -329,6 +329,7 @@ public class BookInfoActivity extends Activity implements MenuItem.OnMenuItemCli
 		item.setOnMenuItemClickListener(this);
 	}
 
+	@Override
 	public boolean onMenuItemClick(MenuItem item) {
 		switch (item.getItemId()) {
 			case OPEN_BOOK:
