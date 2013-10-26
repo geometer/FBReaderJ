16250c29d33c4375c1bbb36b8a7b43fe7b08bf6a
Nikolay Pultsin <geometer@fbreader.org>
2013-10-27 01:16:32 +0200

    new branches

--- scripts/mergeAll.sh
+++ scripts/mergeAll.sh
@@ -13,6 +13,13 @@ git checkout beta-ics
 git merge beta
 git merge ice-cream-sandwich
 
+git checkout plugins
+git merge master
+
+git checkout plugins-ics
+git merge plugins
+git merge ice-cream-sandwich
+
 git checkout yota
 git merge ice-cream-sandwich
 
