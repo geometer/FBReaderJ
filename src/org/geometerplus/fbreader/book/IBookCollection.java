8e7503d164d34fcccd2017ad33742b74336dc524
Nikolay Pultsin <geometer@fbreader.org>
2013-10-31 03:19:48 +0000

    fixed book info saving

--- src/org/geometerplus/fbreader/book/IBookCollection.java
+++ src/org/geometerplus/fbreader/book/IBookCollection.java
@@ -70,7 +70,7 @@ public interface IBookCollection {
 	List<Tag> tags();
 	List<String> firstTitleLetters();
 
-	boolean saveBook(Book book, boolean force);
+	boolean saveBook(Book book);
 	void removeBook(Book book, boolean deleteFromDisk);
 
 	ZLTextPosition getStoredPosition(long bookId);
