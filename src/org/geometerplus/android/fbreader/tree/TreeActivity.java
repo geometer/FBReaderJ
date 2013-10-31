8b02ed43c5aea042e1b61050ec8d1c8447717436
Nikolay Pultsin <geometer@fbreader.org>
2013-10-31 01:21:09 +0000

    fixed library/network library tree updating after book/network book info updating

--- src/org/geometerplus/android/fbreader/tree/TreeActivity.java
+++ src/org/geometerplus/android/fbreader/tree/TreeActivity.java
@@ -162,7 +162,7 @@ public abstract class TreeActivity<T extends FBTree> extends ListActivity {
 		// because key might be null
 		myCurrentKey = myCurrentTree.getUniqueKey();
 		final TreeAdapter adapter = getListAdapter();
-		adapter.replaceAll(myCurrentTree.subtrees());
+		adapter.replaceAll(myCurrentTree.subtrees(), false);
 		setTitle(myCurrentTree.getTreeTitle());
 		final FBTree selectedTree =
 			selectedKey != null ? getTreeByKey(selectedKey) : adapter.getFirstSelectedItem();
