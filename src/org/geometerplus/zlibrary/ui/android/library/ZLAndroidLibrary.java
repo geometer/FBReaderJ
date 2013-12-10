e307ce5e52bbb494632cc429b89b01bfce016c22
Nikolay Pultsin <geometer@fbreader.org>
2013-12-10 04:39:18 +0000

    fixed ZLAndroidLibrary.isYotaPhone() method

--- src/org/geometerplus/zlibrary/ui/android/library/ZLAndroidLibrary.java
+++ src/org/geometerplus/zlibrary/ui/android/library/ZLAndroidLibrary.java
@@ -61,7 +61,7 @@ public final class ZLAndroidLibrary extends ZLibrary {
 	}
 
 	public boolean isYotaPhone() {
-		return true || "YotaPhone".equals(Build.BRAND);
+		return "YotaPhone".equals(Build.BRAND);
 	}
 
 	public boolean hasButtonLightsBug() {
