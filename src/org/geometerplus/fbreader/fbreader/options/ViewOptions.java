c02952a36b2192b78def8e60c2130d8b6168f3e4
Nikolay Pultsin <geometer@fbreader.org>
2013-12-28 17:23:43 +0200

    formatting

--- src/org/geometerplus/fbreader/fbreader/options/ViewOptions.java
+++ src/org/geometerplus/fbreader/fbreader/options/ViewOptions.java
@@ -40,12 +40,19 @@ public class ViewOptions {
 		final int y = zlibrary.getHeightInPixels();
 		final int horMargin = Math.min(dpi / 5, Math.min(x, y) / 30);
 
-		TwoColumnView = new ZLBooleanOption("Options", "TwoColumnView", x * x + y * y >= 42 * dpi * dpi);
-		LeftMargin = new ZLIntegerRangeOption("Options", "LeftMargin", 0, 100, horMargin);
-		RightMargin = new ZLIntegerRangeOption("Options", "RightMargin", 0, 100, horMargin);
-		TopMargin = new ZLIntegerRangeOption("Options", "TopMargin", 0, 100, 15);
-		BottomMargin = new ZLIntegerRangeOption("Options", "BottomMargin", 0, 100, 20);
-		SpaceBetweenColumns = new ZLIntegerRangeOption("Options", "SpaceBetweenColumns", 0, 300, 3 * horMargin);
-		FooterHeight = new ZLIntegerRangeOption("Options", "FooterHeight", 8, dpi / 8, dpi / 20);
+		TwoColumnView =
+			new ZLBooleanOption("Options", "TwoColumnView", x * x + y * y >= 42 * dpi * dpi);
+		LeftMargin =
+			new ZLIntegerRangeOption("Options", "LeftMargin", 0, 100, horMargin);
+		RightMargin =
+			new ZLIntegerRangeOption("Options", "RightMargin", 0, 100, horMargin);
+		TopMargin =
+			new ZLIntegerRangeOption("Options", "TopMargin", 0, 100, 15);
+		BottomMargin =
+			new ZLIntegerRangeOption("Options", "BottomMargin", 0, 100, 20);
+		SpaceBetweenColumns =
+			new ZLIntegerRangeOption("Options", "SpaceBetweenColumns", 0, 300, 3 * horMargin);
+		FooterHeight =
+			new ZLIntegerRangeOption("Options", "FooterHeight", 8, dpi / 8, dpi / 20);
 	}
 }
