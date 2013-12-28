f075c5f9cc7afa9328eec890933f79085987fc8e
Nikolay Pultsin <geometer@fbreader.org>
2013-12-28 17:22:35 +0200

    more options moved to ViewOptions class

--- src/org/geometerplus/fbreader/fbreader/FBView.java
+++ src/org/geometerplus/fbreader/fbreader/FBView.java
@@ -603,7 +603,7 @@ public final class FBView extends ZLTextView {
 
 	@Override
 	public Footer getFooterArea() {
-		if (myReader.ScrollbarTypeOption.getValue() == SCROLLBAR_SHOW_AS_FOOTER) {
+		if (myReader.ViewOptions.ScrollbarType.getValue() == SCROLLBAR_SHOW_AS_FOOTER) {
 			if (myFooter == null) {
 				myFooter = new Footer();
 				myReader.addTimerTask(myFooter.UpdateTask, 15000);
@@ -645,7 +645,7 @@ public final class FBView extends ZLTextView {
 
 	@Override
 	public int scrollbarType() {
-		return myReader.ScrollbarTypeOption.getValue();
+		return myReader.ViewOptions.ScrollbarType.getValue();
 	}
 
 	@Override
