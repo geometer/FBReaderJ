f2388aab9ebaed68cc580f6dc5354112063ad468
Nikolay Pultsin <geometer@fbreader.org>
2014-01-31 19:17:06 +0000

    missing negation

--- src/org/geometerplus/fbreader/network/urlInfo/BookUrlInfo.java
+++ src/org/geometerplus/fbreader/network/urlInfo/BookUrlInfo.java
@@ -135,7 +135,7 @@ public class BookUrlInfo extends UrlInfo {
 		}
 
 		String ext = null;
-		if (format != null && MiscUtil.isEmptyString(format.Extension)) {
+		if (format != null && !MiscUtil.isEmptyString(format.Extension)) {
 			ext = "." + format.Extension;
 		}
 		if (ext == null) {
