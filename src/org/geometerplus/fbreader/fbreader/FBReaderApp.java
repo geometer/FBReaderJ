a217fae3e56f1b520cbc5d1c4b32db4824104af7
Nikolay Pultsin <geometer@fbreader.org>
2014-02-01 00:52:46 +0000

    fixed merging issues

--- src/org/geometerplus/fbreader/fbreader/FBReaderApp.java
+++ src/org/geometerplus/fbreader/fbreader/FBReaderApp.java
@@ -219,18 +219,8 @@ public final class FBReaderApp extends ZLApplication {
 		}
 	}
 
-
-<<<<<<< HEAD
 	public static ColorProfile getColorProfile() {
 		return ColorProfile.get(getColorProfileName());
-=======
-	public ColorProfile getColorProfile() {
-		final String name = getColorProfileName();
-		if (myColorProfile == null || !name.equals(myColorProfile.Name)) {
-			myColorProfile = ColorProfile.get(name);
-		}
-		return myColorProfile;
->>>>>>> master
 	}
 
 	public static String getColorProfileName() {
