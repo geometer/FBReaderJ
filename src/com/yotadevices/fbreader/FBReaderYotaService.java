4dbf62977face941e7d3bda5b87559d4f26f30e1
Nikolay Pultsin <geometer@fbreader.org>
2014-03-23 00:53:51 +0200

    compilation issues

--- src/com/yotadevices/fbreader/FBReaderYotaService.java
+++ src/com/yotadevices/fbreader/FBReaderYotaService.java
@@ -7,8 +7,6 @@ import android.os.IBinder;
 public class FBReaderYotaService extends Service {
 	public static final String KEY_BACK_SCREEN_IS_ACTIVE =
 		"com.yotadevices.fbreader.backScreenIsActive";
-	public static final String KEY_CURRENT_BOOK =
-		"com.yotadevices.fbreader.currentBook";
 
 	public IBinder onBind(Intent intent) {
 		return null;
