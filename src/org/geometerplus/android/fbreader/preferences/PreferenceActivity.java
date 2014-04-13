d98586fd6a28063113d4dbc1edbc1c0586da0f1f
Nikolay Pultsin <geometer@fbreader.org>
2014-04-13 22:35:43 +0100

    cleanup

--- src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
@@ -165,12 +165,12 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 		 */
 		appearanceScreen.addOption(androidLibrary.ShowStatusBarOption, "showStatusBar");
 		appearanceScreen.addOption(androidLibrary.DisableButtonLightsOption, "disableButtonLights");
-		
+
 		if (DeviceType.Instance().isEInk()) {
 			final EInkOptions einkOptions = new EInkOptions();
 			final Screen einkScreen = createPreferenceScreen("eink");
 			final ZLPreferenceSet einkPreferences = new ZLPreferenceSet();
-			
+
 			einkScreen.addPreference(new ZLBooleanPreference(
 				this, einkOptions.EnableFastRefresh, einkScreen.Resource, "enableFastRefresh"
 			) {
@@ -180,12 +180,12 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 					einkPreferences.setEnabled(einkOptions.EnableFastRefresh.getValue());
 				}
 			});
-	
+
 			final ZLIntegerRangePreference updateIntervalPreference = new ZLIntegerRangePreference(
 				this, einkScreen.Resource.getResource("interval"), einkOptions.UpdateInterval
 			);
 			einkScreen.addPreference(updateIntervalPreference);
-	
+
 			einkPreferences.add(updateIntervalPreference);
 			einkPreferences.setEnabled(einkOptions.EnableFastRefresh.getValue());
 		}
