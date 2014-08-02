7fee481e4707b9ad9b23fac1c6238fbbf5f83cd4
Nikolay Pultsin <geometer@fbreader.org>
2014-08-02 17:13:22 +0100

    sync site domain defined in SyncOptions

--- src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
@@ -151,7 +151,7 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 					public void run() {
 						try {
 							myNetworkContext.perform(
-								new JsonRequest("https://demo.fbreader.org/login/test") {
+								new JsonRequest(SyncOptions.URL + "login/test") {
 									@Override
 									public void processResponse(Object response) {
 										// TODO: update message
