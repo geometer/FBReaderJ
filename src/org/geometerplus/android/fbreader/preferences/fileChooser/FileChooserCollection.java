2cf28e8d4476345c95f7fc76663d47710fcf2035
Nikolay Pultsin <geometer@fbreader.org>
2014-08-02 06:18:25 +0100

    select account and authenticate in preferences activity

--- src/org/geometerplus/android/fbreader/preferences/fileChooser/FileChooserCollection.java
+++ src/org/geometerplus/android/fbreader/preferences/fileChooser/FileChooserCollection.java
@@ -31,15 +31,17 @@ import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 public class FileChooserCollection {
 	private final Context myContext;
+	private final int myBaseRequestCode;
 	private final List<FileChooserPreference> myPreferences = new ArrayList<FileChooserPreference>();
 
-	public FileChooserCollection(Context context) {
+	public FileChooserCollection(Context context, int baseRequestCode) {
 		myContext = context;
+		myBaseRequestCode = baseRequestCode;
 	}
 
 	public FileChooserPreference createPreference(ZLResource rootResource, String resourceKey, ZLStringListOption option, Runnable onValueSetAction) {
 		final FileChooserPreference preference = new FileChooserStringListPreference(
-			myContext, rootResource, resourceKey, option, myPreferences.size(), onValueSetAction
+			myContext, rootResource, resourceKey, option, myBaseRequestCode + myPreferences.size(), onValueSetAction
 		);
 		myPreferences.add(preference);
 		return preference;
@@ -47,15 +49,15 @@ public class FileChooserCollection {
 
 	public FileChooserPreference createPreference(ZLResource rootResource, String resourceKey, ZLStringOption option, Runnable onValueSetAction) {
 		final FileChooserPreference preference = new FileChooserStringPreference(
-			myContext, rootResource, resourceKey, option, myPreferences.size(), onValueSetAction
+			myContext, rootResource, resourceKey, option, myBaseRequestCode + myPreferences.size(), onValueSetAction
 		);
 		myPreferences.add(preference);
 		return preference;
 	}
 
-	public void update(int index, Intent data) {
+	public void update(int requestCode, Intent data) {
 		try {
-			myPreferences.get(index).setValueFromIntent(data);
+			myPreferences.get(requestCode - myBaseRequestCode).setValueFromIntent(data);
 		} catch (Exception e) {
 			// ignore
 		}
