6ce833820f6d090713b5d2360893b8e9d6d97e1b
Nikolay Pultsin <geometer@fbreader.org>
2014-08-07 13:58:39 +0100

    formatting

--- src/org/geometerplus/fbreader/book/BookCollection.java
+++ src/org/geometerplus/fbreader/book/BookCollection.java
@@ -25,7 +25,9 @@ import java.util.*;
 import org.geometerplus.zlibrary.core.filesystem.ZLFile;
 import org.geometerplus.zlibrary.core.filesystem.ZLPhysicalFile;
 import org.geometerplus.zlibrary.core.image.ZLImage;
+
 import org.geometerplus.zlibrary.text.view.ZLTextPosition;
+
 import org.geometerplus.fbreader.bookmodel.BookReadingException;
 import org.geometerplus.fbreader.formats.*;
 
