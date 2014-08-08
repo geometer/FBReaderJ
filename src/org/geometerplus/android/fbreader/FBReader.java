47d27897c3c7e1fad33e995cc3e6a6a1b904b232
Nikolay Pultsin <geometer@fbreader.org>
2014-08-08 09:41:30 +0100

    unused config group

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -229,7 +229,6 @@ public final class FBReader extends Activity implements ZLApplicationWindow {
 				config.requestAllValuesForGroup("Fonts");
 				config.requestAllValuesForGroup("Colors");
 				config.requestAllValuesForGroup("Files");
-				config.requestAllValuesForGroup("Security");
 			}
 		});
 
