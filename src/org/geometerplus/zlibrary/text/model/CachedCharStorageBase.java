1905c6c9999abbdb92f73790898a37aca0b5c3f6
Nikolay Pultsin <geometer@fbreader.org>
2014-10-28 10:27:52 +0200

    fixed OOB

--- src/org/geometerplus/zlibrary/text/model/CachedCharStorageBase.java
+++ src/org/geometerplus/zlibrary/text/model/CachedCharStorageBase.java
@@ -44,7 +44,7 @@ abstract class CachedCharStorageBase implements CharStorage {
 	}
 
 	public char[] block(int index) {
-		if (index < 0 && index >= myArray.size()) {
+		if (index < 0 || index >= myArray.size()) {
 			return null;
 		}
 		char[] block = myArray.get(index).get();
