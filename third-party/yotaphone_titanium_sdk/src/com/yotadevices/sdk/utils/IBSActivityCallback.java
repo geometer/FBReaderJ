059b1c4e865a291be482cd3c08fc263af575f097
Yuriy Bereza <yuriy.bereza@yotadevices.com>
2014-12-19 10:17:10 +0300

    added missed sdk files

---
+++ third-party/yotaphone_titanium_sdk/src/com/yotadevices/sdk/utils/IBSActivityCallback.java
@@ -0,0 +1,12 @@
+package com.yotadevices.sdk.utils;
+
+import android.content.ComponentName;
+
+/**
+ * @hide
+ */
+public interface IBSActivityCallback {
+
+    public void onTopBSActivityReturned(ComponentName componentName);
+
+}
---
+++ third-party/yotaphone_titanium_sdk/src/com/yotadevices/sdk/utils/ILockCallback.java
@@ -0,0 +1,12 @@
+package com.yotadevices.sdk.utils;
+
+import android.content.ComponentName;
+
+/**
+ * @hide
+ */
+public interface ILockCallback {
+
+    public void onLockScreenDisabled(boolean isLockScreenDisabled);
+
+}
