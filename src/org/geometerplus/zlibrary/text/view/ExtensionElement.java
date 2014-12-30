59c47a735669dec54db3d0a420091eb1326ea2d2
Nikolay Pultsin <geometer@fbreader.org>
2014-12-30 02:07:26 +0000

    introduced ExtensionElement

--- src/org/geometerplus/zlibrary/text/view/ExtensionElement.java
+++ src/org/geometerplus/zlibrary/text/view/ExtensionElement.java
@@ -19,15 +19,11 @@
 
 package org.geometerplus.zlibrary.text.view;
 
-import java.util.List;
-import java.util.Map;
+import org.geometerplus.zlibrary.core.view.ZLPaintContext;
 
-import org.geometerplus.zlibrary.text.model.ExtensionEntry;
+public abstract class ExtensionElement extends ZLTextElement {
+	protected abstract int getWidth(ZLTextViewBase view);
+	protected abstract int getHeight(ZLTextViewBase view);
 
-public abstract class ExtensionElementManager {
-	final List<? extends ExtensionElement> getElements(ExtensionEntry entry) {
-		return getElements(entry.Type, entry.Data);
-	}
-
-	protected abstract List<? extends ExtensionElement> getElements(String type, Map<String,String> data);
+	protected abstract void draw(ZLPaintContext context, ZLTextView view, ZLTextElementArea area);
 }
