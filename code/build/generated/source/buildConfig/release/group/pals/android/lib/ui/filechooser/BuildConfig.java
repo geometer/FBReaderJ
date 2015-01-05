a837c97775c86353a86a832b0f24020ed22989ad
Steven Huang <stevenhuang1995@gmail.com>
2015-01-05 12:28:18 -0800

    Imported and converted the original Eclipse project into an Android Studio project using Android Studio.
    Compiles and runs after manual compilation of NDK sources.

---
+++ code/build/generated/source/buildConfig/release/group/pals/android/lib/ui/filechooser/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package group.pals.android.lib.ui.filechooser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "group.pals.android.lib.ui.filechooser";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 44;
+  public static final String VERSION_NAME = "";
+}
