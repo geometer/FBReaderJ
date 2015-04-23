de3461389cfc60b70ccb25ff550e68857aa9d10d
Nikolay Pultsin <geometer@fbreader.org>
2015-04-23 03:00:19 +0100

    better toast management

--- src/org/geometerplus/fbreader/fbreader/ActionCode.java
+++ src/org/geometerplus/fbreader/fbreader/ActionCode.java
@@ -80,6 +80,7 @@ public interface ActionCode {
 
 	String OPEN_VIDEO = "video";
 
+	String HIDE_TOAST = "hideToast";
 	String OPEN_START_SCREEN = "openStartScreen";
 	String OPEN_WEB_HELP = "help";
 	String INSTALL_PLUGINS = "plugins";
