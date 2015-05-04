e2ea97fb92a8e0bdfad5cd40b7b8a596f8ac9188
Nikolay Pultsin <geometer@fbreader.org>
2015-05-04 12:01:18 +0100

    removed obsolete branches

--- scripts/mergeAll.sh
+++ scripts/mergeAll.sh
@@ -6,13 +6,6 @@ git merge master
 git checkout ice-cream-sandwich
 git merge master
 
-git checkout beta
-git merge master
-
-git checkout beta-ics
-git merge beta
-git merge ice-cream-sandwich
-
 git checkout yota2
 git merge ice-cream-sandwich
 
@@ -26,11 +19,9 @@ git checkout microtypography
 git merge ice-cream-sandwich
 
 git checkout yotabeta
-git merge beta-ics
 git merge yota2
 
 git checkout betayota
-git merge beta-ics
 git merge yota2
 
 git checkout master
