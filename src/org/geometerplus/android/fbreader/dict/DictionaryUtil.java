e0099f9ec1ea1efd229144235c4c66c264ca635d
Nikolay Pultsin <geometer@fbreader.org>
2015-06-12 15:45:18 +0100

    separate class for Dictan

--- src/org/geometerplus/android/fbreader/dict/DictionaryUtil.java
+++ src/org/geometerplus/android/fbreader/dict/DictionaryUtil.java
@@ -64,8 +64,6 @@ public abstract class DictionaryUtil {
 	private static int FLAG_SHOW_AS_DICTIONARY = 2;
 	private static int FLAG_SHOW_AS_TRANSLATOR = 4;
 
-	private static final int MAX_LENGTH_FOR_TOAST = 180;
-
 	private static ZLStringOption ourSingleWordTranslatorOption;
 	private static ZLStringOption ourMultiWordTranslatorOption;
 
@@ -121,6 +119,10 @@ public abstract class DictionaryUtil {
 			}
 		}
 
+		void onActivityResult(FBReader fbreader, int resultCode, final Intent data) {
+			// does nothing; implement in subclasses
+		}
+
 		abstract void open(String text, Runnable outliner, FBReader fbreader, PopupFrameMetric frameMetrics);
 	}
 
@@ -155,31 +157,6 @@ public abstract class DictionaryUtil {
 				fbreader.startActivity(intent);
 				fbreader.overridePendingTransition(0, 0);
 			} catch (ActivityNotFoundException e) {
-				installDictionaryIfNotInstalled(fbreader, this);
-			}
-		}
-	}
-
-	private static class DictanPackageInfo extends PackageInfo {
-		DictanPackageInfo(String id, String title, boolean supportsTargetLanguageSetting) {
-			super(id, title, supportsTargetLanguageSetting);
-		}
-
-		@Override
-		void open(String text, Runnable outliner, FBReader fbreader, PopupFrameMetric frameMetrics) {
-			final Intent intent = getDictionaryIntent(text);
-			try {
-				intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-				intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-				intent.putExtra("article.mode", 20);
-				intent.putExtra("article.text.size.max", MAX_LENGTH_FOR_TOAST);
-				fbreader.startActivityForResult(intent, FBReader.REQUEST_DICTIONARY);
-				fbreader.overridePendingTransition(0, 0);
-				if (outliner != null) {
-					outliner.run();
-				}
-			} catch (ActivityNotFoundException e) {
-				fbreader.hideOutline();
 				installDictionaryIfNotInstalled(fbreader, this);
 			}
 		}
@@ -228,7 +205,7 @@ public abstract class DictionaryUtil {
 			}
 			final PackageInfo info;
 			if ("dictan".equals(id)) {
-				info = new DictanPackageInfo(id, title, false);
+				info = new Dictan(id, title);
 			} else {
 				info = new PlainPackageInfo(
 					id,
@@ -413,7 +390,7 @@ public abstract class DictionaryUtil {
 		return ourMultiWordTranslatorOption;
 	}
 
-	private static PackageInfo getInfo(String id) {
+	private static PackageInfo getDictionaryInfo(String id) {
 		if (id == null) {
 			return firstInfo();
 		}
@@ -431,7 +408,7 @@ public abstract class DictionaryUtil {
 	public static PackageInfo getCurrentDictionaryInfo(boolean singleWord) {
 		final ZLStringOption option = singleWord
 			? singleWordTranslatorOption() : multiWordTranslatorOption();
-		return getInfo(option.getValue());
+		return getDictionaryInfo(option.getValue());
 	}
 
 	public static class PopupFrameMetric {
@@ -511,116 +488,6 @@ public abstract class DictionaryUtil {
 	}
 
 	public static void onActivityResult(final FBReader fbreader, int resultCode, final Intent data) {
-		if (data == null) {
-			fbreader.hideOutline();
-			return;
-		}
-
-		final int errorCode = data.getIntExtra("error.code", -1);
-		if (resultCode != Activity.RESULT_OK || errorCode != -1) {
-			showDictanError(fbreader, errorCode, data);
-			return;
-		}
-
-		String text = data.getStringExtra("article.text");
-		if (text == null) {
-			showDictanError(fbreader, -1, data);
-			return;
-		}
-		// a hack for obsolete (before 5.0 beta) dictan versions
-		final int index = text.indexOf("\000");
-		if (index >= 0) {
-			text = text.substring(0, index);
-		}
-		final boolean hasExtraData;
-		if (text.length() == MAX_LENGTH_FOR_TOAST) {
-			text = trimArticle(text);
-			hasExtraData = true;
-		} else {
-			hasExtraData = data.getBooleanExtra("article.resources.contains", false);
-		}
-
-		final SuperActivityToast toast;
-		if (hasExtraData) {
-			toast = new SuperActivityToast(fbreader, SuperToast.Type.BUTTON);
-			toast.setButtonIcon(
-				android.R.drawable.ic_menu_more,
-				ZLResource.resource("footnoteToast").getResource("more").getValue()
-			);
-			toast.setOnClickWrapper(new OnClickWrapper("dict", new SuperToast.OnClickListener() {
-				@Override
-				public void onClick(View view, Parcelable token) {
-					final String word = data.getStringExtra("article.word");
-					final PackageInfo info = getInfo("dictan");
-					final Intent intent = info.getDictionaryIntent(word);
-					try {
-						intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-						intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-						fbreader.startActivity(intent);
-						fbreader.overridePendingTransition(0, 0);
-					} catch (ActivityNotFoundException e) {
-						// ignore
-					}
-				}
-			}));
-		} else {
-			toast = new SuperActivityToast(fbreader, SuperToast.Type.STANDARD);
-		}
-		toast.setText(text);
-		toast.setDuration(20000);
-		toast.setOnDismissWrapper(new OnDismissWrapper("ftnt", new SuperToast.OnDismissListener() {
-			@Override
-			public void onDismiss(View view) {
-				fbreader.hideOutline();
-			}
-		}));
-		fbreader.showToast(toast);
-	}
-
-	private static void showDictanError(final FBReader fbreader, int code, Intent data) {
-		final ZLResource resource = ZLResource.resource("dictanErrors");
-		String message;
-		switch (code) {
-			default:
-				message = data.getStringExtra("error.message");
-				if (message == null) {
-					message = resource.getResource("unknown").getValue();
-				}
-				break;
-			case 100:
-			{
-				final String word = data.getStringExtra("article.word");
-				message = resource.getResource("noArticle").getValue().replaceAll("%s", word);
-				break;
-			}
-			case 130:
-				message = resource.getResource("cannotOpenDictionary").getValue();
-				break;
-			case 131:
-				message = resource.getResource("noDictionarySelected").getValue();
-				break;
-		}
-
-		final SuperActivityToast toast = new SuperActivityToast(fbreader, SuperToast.Type.STANDARD);
-		toast.setText("Dictan: " + message);
-		toast.setDuration(5000);
-		toast.setOnDismissWrapper(new OnDismissWrapper("ftnt", new SuperToast.OnDismissListener() {
-			@Override
-			public void onDismiss(View view) {
-				fbreader.hideOutline();
-			}
-		}));
-		fbreader.showToast(toast);
-	}
-
-	private static String trimArticle(String text) {
-		final int len = text.length();
-		final int eolIndex = text.lastIndexOf("\n");
-		final int spaceIndex = text.lastIndexOf(" ");
-		if (spaceIndex < eolIndex || eolIndex >= len * 2 / 3) {
-			return text.substring(0, eolIndex);
-		} else {
-			return text.substring(0, spaceIndex);
-		}
+		getDictionaryInfo("dictan").onActivityResult(fbreader, resultCode, data);
 	}
 }
