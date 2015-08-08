dc85f0a504e4f884f986a97e265cb61db55ded00
Nikolay Pultsin <geometer@fbreader.org>
2015-08-08 19:49:00 +0200

    ChangeLog + copyright years

--- src/org/geometerplus/android/fbreader/preferences/MenuConfigurationActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/MenuConfigurationActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2015 FBReader.ORG Limited <contact@fbreader.org>
+ * Copyright (C) 2009-2015 FBReader.ORG Limited <contact@fbreader.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
