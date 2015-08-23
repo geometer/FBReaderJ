cf2b810f1270af75984ecf62445273a2b6b54b5b
Danie Roux <danie@danieroux.com>
2015-08-23 22:30:38 +0200

    Enable "Show Progress" option only if the right scrollbar type is selected
    Follows behaviour of other options on page.

--- src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
@@ -557,11 +557,17 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 		newStyleFooterPreferences.add(statusLineScreen.addOption(profile.FooterNGForegroundUnreadOption, "footerForegroundUnreadColor"));
 		footerPreferences.add(statusLineScreen.addOption(footerOptions.ShowTOCMarks, "tocMarks"));
 
-
 		statusLineScreen.addPreference(new ZLChoicePreference(
 				this, statusLineScreen.Resource.getResource("showProgressTypes"),
 				footerOptions.ShowProgressType, footerOptions.getProgressValueResourceKeys()
-		));
+		) {
+			@Override
+			public boolean isEnabled() {
+				int scrollbarType = viewOptions.ScrollbarType.getValue();
+				return scrollbarType == FBView.SCROLLBAR_SHOW_AS_FOOTER ||
+						scrollbarType == FBView.SCROLLBAR_SHOW_AS_FOOTER_OLD_STYLE;
+			}
+		});
 
 		footerPreferences.add(statusLineScreen.addOption(footerOptions.ShowClock, "showClock"));
 		footerPreferences.add(statusLineScreen.addOption(footerOptions.ShowBattery, "showBattery"));
