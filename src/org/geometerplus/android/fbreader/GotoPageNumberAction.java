e0c6a1d51c07791858fdaf6800ce81f2e8dd82c9
Nikolay Pultsin <geometer@fbreader.org>
2015-09-25 20:14:22 +0100

    fixed compilation issue

--- src/org/geometerplus/android/fbreader/GotoPageNumberAction.java
+++ src/org/geometerplus/android/fbreader/GotoPageNumberAction.java
@@ -20,6 +20,7 @@
 package org.geometerplus.android.fbreader;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
